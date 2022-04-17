65f3a8209dc743ae2d94ae3f0239fce7bcdedf72
yunho <kimyouknow@naver.com>
2022-04-17 22:14:21 +0900

    Refactor: cookie utils에  getLoginUserInfo() 함수 생성
    - 로그인된 유저 정보 반환
    - tempUserinfo 생성해서 임시로 로그인된 유저 정보 생성

--- webapp/src/utils/cookie.js
+++ webapp/src/utils/cookie.js
@@ -1,11 +1,18 @@
 import { AUTH_KEY, USER_INFO } from 'constant';
 import { Cookies } from 'react-cookie';
+import { setDefaultProfileImage } from './constant';
 
 const cookies = new Cookies();
 
 export const setCookie = (name, value, option) => cookies.set(name, value, { ...option });
 
 export const getCookie = (name) => cookies.get(name);
+
+export const getLoginUserInfo = () => {
+  const userInfo = cookies.get(USER_INFO);
+  userInfo.img = setDefaultProfileImage(userInfo.img);
+  return userInfo;
+};
 
 export const getAuthCookie = () => cookies.get(AUTH_KEY);
 
@@ -15,3 +22,12 @@ export const removeLoginCookie = () => {
   removeCookie(AUTH_KEY);
   removeCookie(USER_INFO);
 };
+
+const tempUserInfo = {
+  name: '임시',
+  id: 1901941,
+  img: '',
+};
+
+setCookie('userInfo', tempUserInfo);
+// removeCookie('userInfo');
