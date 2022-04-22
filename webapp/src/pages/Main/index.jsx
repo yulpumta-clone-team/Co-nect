6b06f408042c9450320328b2cb23f5e0e260634c
yunho <kimyouknow@naver.com>
2022-04-22 20:04:10 +0900

    Feat: s3 이미지 서버 업로드 구현

--- webapp/src/pages/Main/index.jsx
+++ webapp/src/pages/Main/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import WindowModal from 'components/WindowModal';
 import useModal from 'hooks/useModal';
+import ImageUploader from 'components/ImageUploader';
 
 function Main() {
   const [showModal, onCloseModal, openModal] = useModal();
   return (
     <div>
       <h1>Main</h1>
+      <ImageUploader />
       <button onClick={openModal}>Open Modal</button>
       <WindowModal show={showModal} onCloseModal={onCloseModal}>
         <div>
