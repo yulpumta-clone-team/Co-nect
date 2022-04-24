5a6613d94383c5e3e29a05f74a05ac2b22450119
yunho <kimyouknow@naver.com>
2022-04-24 20:21:57 +0900

    Refactor: useApi, teamApi에서 commentApi 분리

---
+++ webapp/src/api/comment.js
@@ -0,0 +1,77 @@
+import { MOCK_SERVER_URL } from 'constant/route';
+import instance from './core';
+
+const commentApi = {
+  GET_COMMENT({ postType, postId }) {
+    return instance({
+      url: `${MOCK_SERVER_URL}/${postType}/comment/${postId}`,
+      method: 'get',
+    });
+  },
+  POST_COMMENT({ postType, data }) {
+    return instance({
+      url: `/${postType}/comment`,
+      method: 'post',
+      data,
+    });
+  },
+  POST_REPLY({ postType, data }) {
+    return instance({
+      url: `/${postType}/nested_comment`,
+      method: 'post',
+      data,
+    });
+  },
+  DELETE_COMMENT({ postType, comment_id }) {
+    return instance({
+      url: `/${postType}/comment`,
+      method: 'delete',
+      params: { comment_id },
+    });
+  },
+  // DELETE_REPLY() {
+  //   return instance({
+  //     url: '/user/logout',
+  //   });
+  // },
+  PATCH_LIKE({ user_id }) {
+    return instance({
+      url: '/user/liking',
+      method: 'patch',
+      params: { user_id },
+    });
+  },
+  // PATCH_COMMENT_LIKE({ user_id }) {
+  //   return instance({
+  //     url: '/user/liking',
+  //     method: 'patch',
+  //     params: { user_id },
+  //   });
+  // },
+  PATCH_USER_COMMENT({ postType, data }) {
+    return instance({
+      url: '/user/comment',
+      method: 'patch',
+      data,
+    });
+  },
+  PATCH_USER_REPLY({ postType, data }) {
+    return instance({
+      url: '/user/nested_comment',
+      method: 'patch',
+      data,
+    });
+  },
+  // HANDLE_SECRET_USER_REPLY() {
+  //   return instance({
+  //     url: '/user/logout',
+  //   });
+  // },
+  // HANDLE_SECRET_COMMENT() {
+  //   return instance({
+  //     url: '/user/logout',
+  //   });
+  // },
+};
+
+export default commentApi;
--- webapp/src/api/team.js
+++ webapp/src/api/team.js
@@ -22,69 +22,6 @@ const teamApi = {
       method: 'get',
     });
   },
-  GET_TEAM_COMMENT(teamId) {
-    return instance({
-      url: `${MOCK_SERVER_URL}/team/comment/${teamId}`,
-      method: 'get',
-    });
-  },
-  POST_TEAM_COMMENT(data) {
-    return instance({
-      url: '/team/comment',
-      method: 'post',
-      data,
-    });
-  },
-  POST_TEAM_REPLY(data) {
-    return instance({
-      url: '/team/nested_comment',
-      method: 'post',
-      data,
-    });
-  },
-  DELETE_TEAM_COMMENT({ comment_id }) {
-    return instance({
-      url: '/team/comment',
-      method: 'delete',
-      params: { comment_id },
-    });
-  },
-  // DELETE_TEAM_REPLY() {
-  //   return instance({
-  //     url: '/team/logout',
-  //   });
-  // },
-  PATCH_TEAM_LIKE({ team_id }) {
-    return instance({
-      url: '/team/liking',
-      method: 'patch',
-      params: { team_id },
-    });
-  },
-  PATCH_TEAM_COMMENT(data) {
-    return instance({
-      url: '/team/comment',
-      method: 'patch',
-      data,
-    });
-  },
-  PATCH_TEAM_REPLY(data) {
-    return instance({
-      url: '/team/nested_comment',
-      method: 'patch',
-      data,
-    });
-  },
-  // HANDLE_SECRET_TEAM_REPLY() {
-  //   return instance({
-  //     url: '/team/logout',
-  //   });
-  // },
-  // HANDLE_SECRET_TEAM_COMMENT() {
-  //   return instance({
-  //     url: '/team/logout',
-  //   });
-  // },
 };
 
 export default teamApi;
