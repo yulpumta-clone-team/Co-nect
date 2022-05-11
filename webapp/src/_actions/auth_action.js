10a35c670ad2e74cb0ee2b9ec532a28379a5a621
philos1234 <ohhyun1948@gmail.com>
2022-05-12 00:21:32 +0900

    Feat : UserInfo 쿠키 정보 생성 기능 구현

--- webapp/src/_actions/auth_action.js
+++ webapp/src/_actions/auth_action.js
@@ -5,7 +5,11 @@ import { AUTH, LOGIN_USER, SIGNUP_USER } from '_types/authType';
 export function actionLogin(responseData) {
   const { data } = responseData;
   const { data: userInfo } = data;
+<<<<<<< HEAD
   setCookie(USER_INFO, userInfo, { maxAge: 60 * 60, path: '/' });
+=======
+
+>>>>>>> 24a587b (Feat : UserInfo 쿠키 정보 생성 기능 구현)
   return {
     type: LOGIN_USER,
     payload: data,
