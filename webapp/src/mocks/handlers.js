98f8db3d88850c0c36a6d85152e35688ad5841d8
yunho <kimyouknow@naver.com>
2022-05-11 23:40:00 +0900

    Feat: msw Mocking api 추가

--- webapp/src/mocks/handlers.js
+++ webapp/src/mocks/handlers.js
@@ -2,8 +2,11 @@ import { rest } from 'msw';
 import { ROOT_URL } from 'constant/route';
 import mockUsers from './mockUsers';
 import mockUser from './mockUser';
+import mockUserComment from './mockUserComment';
 import mockTeams from './mockTeams';
 import mockTeam from './mockTeam';
+import mockTeamComment from './mockTeamComment';
+import mockEditTeamPost from './mockEditTeamPost';
 
 const USER = [
   rest.get(`${ROOT_URL}/users?page=1`, (req, res, ctx) => {
@@ -11,6 +14,9 @@ const USER = [
   }),
   rest.get(`${ROOT_URL}/user/:id`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(mockUser));
+  }),
+  rest.get(`${ROOT_URL}/user/comment/:id`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockUserComment));
   }),
 ];
 
@@ -20,6 +26,12 @@ const TEAM = [
   }),
   rest.get(`${ROOT_URL}/team/:id`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(mockTeam));
+  }),
+  rest.patch(`${ROOT_URL}/team/:id`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockEditTeamPost));
+  }),
+  rest.get(`${ROOT_URL}/team/comment/:id`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockTeamComment));
   }),
 ];
 
