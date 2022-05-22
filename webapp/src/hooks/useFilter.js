046b25944b91f080cdc27f6cee01ee9420a2712b
yunho <kimyouknow@naver.com>
2022-05-22 15:43:52 +0900

    Chore: 기술스택 contant에서 변수로 관리

--- webapp/src/hooks/useFilter.js
+++ webapp/src/hooks/useFilter.js
@@ -1,27 +1,14 @@
 import { useCallback, useState } from 'react';
+import { languageSkills } from 'utils/constant';
 
 const useFilter = () => {
   const [checked, setChecked] = useState([
     100, 101, 102, 103, 104, 200, 201, 202, 203, 300, 301, 302,
   ]);
-  const filterObj = {
-    100: 'front',
-    101: 'react',
-    102: 'js',
-    103: 'ts',
-    104: 'vue',
-    200: 'back',
-    201: 'nodejs',
-    202: 'java',
-    203: 'spring',
-    300: 'design',
-    301: 'xd',
-    302: 'figma',
-  };
   const handleFilter = useCallback(
     (list) => {
       if (list.length === 0 || !list) return [];
-      const checkedList = checked.map((e) => filterObj[e]);
+      const checkedList = checked.map((e) => languageSkills[e]);
       const newList = [...list].filter(
         ({ skills }) => skills.filter((x) => checkedList.includes(x)).length > 0,
       );
--- webapp/src/utils/constant.js
+++ webapp/src/utils/constant.js
@@ -4,4 +4,29 @@ export function setDefaultProfileImage(img) {
   return (!img || img.length < 10) && DEFAULT_PROFILE_IMG;
 }
 
-export const temp = '';
+export const languageSkills = {
+  100: 'Javascript',
+  101: 'React',
+  102: 'Vue',
+  103: 'Angular',
+  104: 'Svelte',
+  105: 'Typescript',
+  200: 'C',
+  201: 'C++',
+  202: 'C#',
+  203: 'Java',
+  204: 'Spring',
+  205: 'Node.js',
+  206: 'Python',
+  207: 'Django',
+  208: 'Ruby',
+  209: 'Ruby',
+  210: 'Go',
+  300: 'Swift',
+  301: 'Kotlin',
+  302: 'Flutter',
+  303: 'React-Native',
+  304: 'Objective-C',
+  400: 'XD',
+  401: 'Figma',
+};
