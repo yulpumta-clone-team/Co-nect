abed3d0816bbfd3a28be21043f28150609250697
yunho <kimyouknow@naver.com>
2022-05-22 17:28:44 +0900

    Refactor : user,team,comment reducer제거

--- webapp/src/apiAction/team.js
+++ webapp/src/apiAction/team.js
@@ -1,17 +1,12 @@
 import teamApi from 'api/team';
 
-export function getTeamList({ page }) {
-  return teamApi.GET_TEAM_ARR(page).then((response) => response);
-}
+export const getTeamList = ({ page }) => teamApi.GET_TEAM_ARR(page).then((response) => response);
 
-export function getTeamDetail({ id }) {
-  return teamApi.GET_TEAM_DETAIL({ id }).then((response) => response);
-}
+export const getTeamDetail = ({ id }) =>
+  teamApi.GET_TEAM_DETAIL({ id }).then((response) => response);
 
-export function postTeamPost(dataTosubmit) {
-  return teamApi.POST_TEAM_POST({ data: dataTosubmit }).then((response) => response);
-}
+export const postTeamPost = (dataTosubmit) =>
+  teamApi.POST_TEAM_POST({ data: dataTosubmit }).then((response) => response);
 
-export function patchTeamPost({ id, editTeamInfo }) {
-  return teamApi.EDIT_TEAM_POST({ id, data: editTeamInfo }).then((response) => response);
-}
+export const patchTeamPost = ({ id, editTeamInfo }) =>
+  teamApi.EDIT_TEAM_POST({ id, data: editTeamInfo }).then((response) => response);
--- webapp/src/apiAction/user.js
+++ webapp/src/apiAction/user.js
@@ -1,21 +1,13 @@
 import userApi from 'api/user';
 
-export function getUserList({ page }) {
-  return userApi.GET_USER_LIST(page).then((response) => response);
-}
+export const getUserList = ({ page }) => userApi.GET_USER_LIST(page).then((response) => response);
 
-export function getUserDetail({ id }) {
-  return userApi.GET_USER_DETAIL({ id }).then((response) => response);
-}
+export const getUserDetail = ({ id }) =>
+  userApi.GET_USER_DETAIL({ id }).then((response) => response);
 
-export function patchUserProfile({ id }) {
-  return userApi.EDIT_USER_PROFILE({ id }).then((response) => response);
-}
+export const patchUserProfile = ({ id }) =>
+  userApi.EDIT_USER_PROFILE({ id }).then((response) => response);
 
-export function getUserLikeList() {
-  return userApi.GET_USER_LIKES().then((response) => response);
-}
+export const getUserLikeList = () => userApi.GET_USER_LIKES().then((response) => response);
 
-export function getUserReadList() {
-  return userApi.GET_USER_READS().then((response) => response);
-}
+export const getUserReadList = () => userApi.GET_USER_READS().then((response) => response);
