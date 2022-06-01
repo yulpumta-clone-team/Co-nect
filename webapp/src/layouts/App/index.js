f32db3acc65f3ce29c7d7ab260d1f964ad51c48b
yunho <kimyouknow@naver.com>
2022-06-01 21:38:05 +0900

    Refactor: hoc/auth제거

--- webapp/src/layouts/App/index.js
+++ webapp/src/layouts/App/index.js
@@ -33,9 +33,6 @@ import PublicRoute from 'hoc/PublicRoute';
 import PrivateRoute from 'hoc/PrivateRoute';
 import AppLayout from './style';
 
-// option: null => 아무나 출입 가능
-// option: true => 로그인 유저만
-// option: false => 로그인 하면 출입 불가능한 곳(회원가입 등...)
 function App() {
   const { isOpen, errorContent, modalContent } = useSelector((state) => state.global);
   return (
