2d102bf2b1d85402db2d1616b26abcb9172835db
yunho <kimyouknow@naver.com>
2022-07-11 20:51:55 +0900

    Chore: mock handler api별로 주석

--- webapp/src/mocks/team/index.js
+++ webapp/src/mocks/team/index.js
@@ -6,25 +6,32 @@ import teamsList from './teamsList';
 import teamComments from './teamComments';
 
 const TEAM = [
+  // GET_TEAM_ARR
   rest.get(ROOT_URL + API.TEAM.LIST, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(teamsList));
   }),
+  // GET_TEAM_LIKES
   rest.get(`${ROOT_URL + API.TEAM.LIKES}`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(teamsList));
   }),
+  // GET_TEAM_READS
   rest.get(`${ROOT_URL + API.TEAM.READS}`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(teamsList));
   }),
+  // GET_TEAM_DETAIL
+  rest.get(`${ROOT_URL + API.TEAM.DETAIL}/:id`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(teamDetail));
+  }),
+  // POST_TEAM_POST
   rest.post(`${ROOT_URL + API.TEAM.DETAIL}`, (req, res, ctx) => {
     console.log(req.body);
     return res(ctx.status(200), ctx.json(teamDetail));
   }),
-  rest.get(`${ROOT_URL + API.TEAM.DETAIL}/:id`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(teamDetail));
-  }),
+  // EDIT_TEAM_POST
   rest.patch(`${ROOT_URL + API.TEAM.DETAIL}/:id`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(editTeamDetail));
   }),
+  // GET_COMMENT
   rest.get(`${ROOT_URL + API.TEAM.DETAIL + API.COMMENT}/:id`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(teamComments));
   }),
--- webapp/src/mocks/user/index.js
+++ webapp/src/mocks/user/index.js
@@ -5,6 +5,7 @@ import usersList from './usersList';
 import userDetail from './userDetail';
 
 const USER = [
+  // GET_USER_LIST
   rest.get(ROOT_URL + API.USER.LIST, (req, res, ctx) => {
     const lastPage = Number(req.url.searchParams.get('lastPage'));
     if (lastPage >= 2) {
@@ -12,18 +13,27 @@ const USER = [
     }
     return res(ctx.status(200), ctx.json(usersList));
   }),
+  // GET_USER_LIKES
   rest.get(ROOT_URL + API.USER.LIKES, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(usersList));
   }),
+  // GET_USER_READS
   rest.get(ROOT_URL + API.USER.READS, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(usersList));
   }),
+  // GET_MY_POSTS
   rest.get(ROOT_URL + API.USER.MYPOSTS, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(usersList));
   }),
+  // GET_USER_DETAIL
   rest.get(`${ROOT_URL + API.USER.DETAIL}/:id`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(userDetail));
   }),
+  // EDIT_USER_PROFILE
+  rest.patch(`${ROOT_URL + API.USER.DETAIL}/:id`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(userDetail));
+  }),
+  // GET_COMMENT
   rest.get(`${ROOT_URL + API.USER.DETAIL + API.COMMENT}/:id`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(userComments));
   }),
