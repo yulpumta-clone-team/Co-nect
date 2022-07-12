6c442ef03256adc233aa02b2e52d2cc451455c53
yunho <kimyouknow@naver.com>
2022-07-12 21:58:21 +0900

    Refactor: team mock에 getResonseWithData적용

--- webapp/src/mocks/team/index.js
+++ webapp/src/mocks/team/index.js
@@ -1,39 +1,35 @@
 import { API, ROOT_URL } from 'constant/api';
+import { getResonseWithData } from 'mocks/mockUtils';
 import { rest } from 'msw';
-import editTeamDetail from './editTeamDetail';
-import teamDetail from './teamDetail';
-import teamsList from './teamsList';
-import teamComments from './teamComments';
+import { editTeamDetail } from './editTeamDetail';
+import { teamDetail } from './teamDetail';
+import { teamsList } from './teamsList';
 
 const TEAM = [
   // GET_TEAM_ARR
   rest.get(ROOT_URL + API.TEAM.LIST, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(teamsList));
+    return res(ctx.status(200), ctx.json(getResonseWithData(teamsList)));
   }),
   // GET_TEAM_LIKES
   rest.get(`${ROOT_URL + API.TEAM.LIKES}`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(teamsList));
+    return res(ctx.status(200), ctx.json(getResonseWithData(teamsList)));
   }),
   // GET_TEAM_READS
   rest.get(`${ROOT_URL + API.TEAM.READS}`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(teamsList));
+    return res(ctx.status(200), ctx.json(getResonseWithData(teamsList)));
   }),
   // GET_TEAM_DETAIL
   rest.get(`${ROOT_URL + API.TEAM.DETAIL}/:id`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(teamDetail));
+    return res(ctx.status(200), ctx.json(getResonseWithData(teamDetail)));
   }),
   // POST_TEAM_POST
   rest.post(`${ROOT_URL + API.TEAM.DETAIL}`, (req, res, ctx) => {
     console.log(req.body);
-    return res(ctx.status(200), ctx.json(teamDetail));
+    return res(ctx.status(200), ctx.json(getResonseWithData(teamDetail)));
   }),
   // EDIT_TEAM_POST
   rest.patch(`${ROOT_URL + API.TEAM.DETAIL}/:id`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(editTeamDetail));
-  }),
-  // GET_COMMENT
-  rest.get(`${ROOT_URL + API.TEAM.DETAIL + API.COMMENT}/:id`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(teamComments));
+    return res(ctx.status(200), ctx.json(getResonseWithData(editTeamDetail)));
   }),
 ];
 
