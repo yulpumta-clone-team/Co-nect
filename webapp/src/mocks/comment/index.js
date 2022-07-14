7f707699698b6a056ff5e6b543819345a786723d
yunho <kimyouknow@naver.com>
2022-07-14 21:56:17 +0900

    Feat: mock user comment api crud 세팅

--- webapp/src/mocks/comment/index.js
+++ webapp/src/mocks/comment/index.js
@@ -7,23 +7,42 @@ import { userComment, userComments } from './userComments';
 const COMMENT = [
   // GET_USER_COMMENT
   rest.get(`${ROOT_URL + API.USER.DETAIL + API.COMMENT.ORIGIN}/:id`, (req, res, ctx) => {
+    console.log('request GET_USER_COMMENT');
     return res(ctx.status(200), ctx.json(getResonseWithData(userComments)));
   }),
   // POST_USER_COMMENT
   rest.post(`${ROOT_URL + API.USER.DETAIL + API.COMMENT.ORIGIN}`, (req, res, ctx) => {
+    console.log('request POST_USER_COMMENT');
     const newUserComments = [userComment, ...userComments];
     return res(ctx.status(200), ctx.json(getResonseWithData(newUserComments)));
   }),
   // DELETE_USER_COMMENT
   rest.delete(`${ROOT_URL + API.USER.DETAIL + API.COMMENT.ORIGIN}/:id`, (req, res, ctx) => {
+    console.log('request DELETE_USER_COMMENT');
     return res(ctx.status(200), ctx.json(getResonseWithData(userComments)));
   }),
   // PATCH_USER_COMMENT
   rest.patch(`${ROOT_URL + API.USER.DETAIL + API.COMMENT.ORIGIN}/:id`, (req, res, ctx) => {
+    console.log('request PATCH_USER_COMMENT');
+    const { body, params } = req;
     return res(ctx.status(200), ctx.json(getResonseWithData(userComments)));
   }),
   // POST_USER_REPLY
+  rest.post(`${ROOT_URL + API.USER.DETAIL + API.COMMENT.ORIGIN}/:id`, (req, res, ctx) => {
+    console.log('request POST_USER_REPLY');
+    return res(ctx.status(200), ctx.json(getResonseWithData(userComments)));
+  }),
+  // DELETE_USER_REPLY
+  rest.delete(`${ROOT_URL + API.USER.DETAIL + API.COMMENT.ORIGIN}/:id`, (req, res, ctx) => {
+    console.log('request DELETE_USER_REPLY');
+    return res(ctx.status(200), ctx.json(getResonseWithData(userComments)));
+  }),
   // PATCH_USER_REPLY
+  rest.patch(`${ROOT_URL + API.USER.DETAIL + API.COMMENT.ORIGIN}/:id`, (req, res, ctx) => {
+    console.log('request PATCH_USER_REPLY');
+    return res(ctx.status(200), ctx.json(getResonseWithData(userComments)));
+  }),
+
   // PATCH_USER_COMMENT_LIKE
   // PATCH_USER_COMMENT_UN_LIKE
 
