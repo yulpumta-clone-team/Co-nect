6945d34268cceed04084d75187d708012bc8e7cc
yunho <kimyouknow@naver.com>
2022-07-18 21:21:47 +0900

    Feat: useDropdown hooks

---
+++ webapp/src/hooks/useDropdown.js
@@ -0,0 +1,29 @@
+import { useState, useEffect, useRef } from 'react';
+
+const useDropdown = (initialMode = false) => {
+  const parent = useRef();
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(initialMode);
+
+  const shouldCloseDropdown = (event) => {
+    // console.log('event', event.composedPath());
+    const isParentExistInComposedPath = event.composedPath().includes(parent.current);
+    if (isParentExistInComposedPath) return;
+    closeDropdown();
+  };
+
+  const closeDropdown = () => setIsDropdownOpen(false);
+
+  const openDropdown = () => setIsDropdownOpen(true);
+
+  useEffect(() => {
+    window.addEventListener('click', shouldCloseDropdown, true);
+
+    return () => {
+      window.removeEventListener('click', shouldCloseDropdown, true);
+    };
+  }, [parent]);
+  return [parent, isDropdownOpen, shouldCloseDropdown, openDropdown, closeDropdown];
+};
+
+export default useDropdown;
