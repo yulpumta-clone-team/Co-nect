c78f5eebe96e7bc64ccb793ff8cf7fc70bc2b684
YUNHO <kimyouknow@naver.com>
2022-07-18 22:29:30 +0900

    [FE] dropdown 공통 모달 UI, hooks 구현 및 적용 (#88)
    * Refactor: Menu컴포넌트 제거
    
    * Refactor: WindowModal, Dropdown Common컴포넌트로 이동
    
    * Feat: useDropdown hooks
    
    * Refactor:  유저인포dropdown컴포넌트 구현
    
    * Style: GBN 스타일컴포넌트 네이밍 변경
    
    * Refactor: UserInfoDropdown의 ref 프로필이미지로 변경
    
    * Feat: Dropdown컴포넌트에 custonmstyle 상속받아서 사용할 수 있게 하기
    
    * Feat: Dropdown컴포넌트 내부 레이아웃에 click.stopPropagation 적용
    
    * Rename: triggerLogOut -> onClickLogout
    
    * Rename: dropdown container -> overlay, layout-> content
    
    * Style: dropdown컴포넌트 default스타일 객체로 분리
    
    * Chore: 주석정리

---
+++ webapp/src/components/Common/Dropdown/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import * as S from './style';
+
+Dropdown.propTypes = {
+  children: PropTypes.element.isRequired,
+  isDropdownOpen: PropTypes.bool.isRequired,
+  customStyle: PropTypes.shape({
+    overlayStyle: PropTypes.object,
+    contentStyle: PropTypes.object,
+    buttonStyle: PropTypes.object,
+  }),
+};
+
+export default function Dropdown({ children, isDropdownOpen, customStyle }) {
+  if (!isDropdownOpen) return null;
+  return (
+    <S.Overlay customStyle={customStyle?.overlayStyle}>
+      <S.Content
+        onClick={(event) => event.stopPropagation()}
+        customStyle={customStyle?.contentStyle}
+      >
+        {children}
+      </S.Content>
+    </S.Overlay>
+  );
+}
