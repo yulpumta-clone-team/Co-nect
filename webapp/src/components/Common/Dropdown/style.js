010a7f8344565aa6103adc7b6d6a0cb7c4ec463a
yunho <kimyouknow@naver.com>
2022-07-18 21:55:51 +0900

    Style: dropdown컴포넌트 default스타일 객체로 분리

--- webapp/src/components/Common/Dropdown/style.js
+++ webapp/src/components/Common/Dropdown/style.js
@@ -1,14 +1,40 @@
 import styled, { css } from 'styled-components';
 
-export const Overlay = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 1px;
+export const defaultStyle = {
+  overlay: css`
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    border-radius: 16px;
+    border: 1px solid #d9dbe9;
+    z-index: 99;
+  `,
+  content: css`
+    display: flex;
+    align-items: center;
+    padding: 8px 16px;
+    gap: 8px;
 
-  position: absolute;
-  border-radius: 16px;
-  border: 1px solid #d9dbe9;
+    width: 240px;
+    height: 100px;
+
+    background-color: #fefefe;
+    border: 1px solid #d9dbe9;
+  `,
+  button: css`
+    position: absolute;
+    right: 10px;
+    top: 6px;
+    background: transparent;
+    border: none;
+    z-index: 512;
+    font-size: 12px;
+    cursor: pointer;
+  `,
+};
+
+export const Overlay = styled.div`
+  ${defaultStyle.overlay};
   ${({ customStyle }) =>
     customStyle &&
     css`
@@ -17,16 +43,7 @@ export const Overlay = styled.div`
 `;
 
 export const Content = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 8px 16px;
-  gap: 8px;
-
-  width: 240px;
-  height: 100px;
-
-  background-color: #fefefe;
-  border: 1px solid #d9dbe9;
+  ${defaultStyle.content}
   ${({ customStyle }) =>
     customStyle &&
     css`
@@ -35,14 +52,7 @@ export const Content = styled.div`
 `;
 
 export const CloseButton = styled.button`
-  position: absolute;
-  right: 10px;
-  top: 6px;
-  background: transparent;
-  border: none;
-  z-index: 512;
-  font-size: 12px;
-  cursor: pointer;
+  ${defaultStyle.button}
   ${({ customStyle }) =>
     customStyle &&
     css`
