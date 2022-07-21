fbf6a9ba8a93bdc4bcbf6ab528e8b34a354eaade
yunho <kimyouknow@naver.com>
2022-07-21 21:12:45 +0900

    Refactor: WithLoading 컴포넌트 Props 추가
      Component,
      responseDataKey,
      axiosInstance,
      axiosConfig,
      LoadingFallback,
      ErrorFallback,

--- webapp/src/hoc/WithLoading.jsx
+++ webapp/src/hoc/WithLoading.jsx
@@ -1,25 +1,38 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import PropTypes from 'prop-types';
 import useAxios from 'hooks/useAxios';
 
+// ! propTypes가 적용이 안 됨.
 WithLoading.propTypes = {
   Component: PropTypes.element.isRequired,
-  fetcher: PropTypes.func.isRequired,
-  LoadingComponent: PropTypes.element.isRequired,
+  responseDataKey: PropTypes.string.isRequired,
+  axiosInstance: PropTypes.func.isRequired,
+  axiosConfig: PropTypes.object.isRequired,
+  LoadingFallback: PropTypes.element.isRequired,
+  ErrorFallback: PropTypes.element.isRequired,
 };
 
-export default function WithLoading({ Component, LoadingComponent, fetcher }) {
-  const [responseData, isLoading, error, refetch] = useAxios();
+export default function WithLoading({
+  Component,
+  responseDataKey,
+  axiosInstance,
+  axiosConfig,
+  LoadingFallback,
+  ErrorFallback,
+}) {
+  return function Wrapper(props) {
+    const [responseData, isLoading, error, forceRefetch] = useAxios(axiosInstance, axiosConfig);
 
-  if (isLoading) return <div>Loading....</div>;
+    if (isLoading) return <div>Loading....</div>;
 
-  if (error.isError)
-    return (
-      <div>
-        {error.msg}
-        <button onClick={refetch}>refetch</button>
-      </div>
-    );
-
-  return <Component data={responseData} />;
+    if (error.isError)
+      return (
+        <div>
+          <button onClick={forceRefetch}>refetch</button>
+        </div>
+      );
+    const propsWithResponseData = { ...props, [responseDataKey]: responseData };
+    return <Component {...propsWithResponseData} />;
+  };
 }
