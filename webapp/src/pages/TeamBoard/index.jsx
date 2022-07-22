f1de55ba8a13f182c85e0cf56e5972cfa5614b13
yunho <kimyouknow@naver.com>
2022-07-22 22:20:15 +0900

    Refactor: WithInfiniteScroll -> WithLoading처럼 변경

--- webapp/src/pages/TeamBoard/index.jsx
+++ webapp/src/pages/TeamBoard/index.jsx
@@ -3,16 +3,18 @@ import teamApi from 'api/team';
 import TeamCard from 'components/TeamCard';
 import { TEAM } from 'constant/route';
 import WithInfiniteScroll from 'hoc/WithInfiniteScroll';
+import CardsGrid from 'components/CardsGrid';
 import * as S from './style';
 
 export default function TeamBoard() {
+  const UserCardsGridWithInfiniteScroll = WithInfiniteScroll({
+    Component: CardsGrid,
+    responseDataKey: 'cards',
+    axiosInstance: teamApi.GET_TEAM_LIST,
+  });
   return (
     <S.BoardWrapper>
-      <WithInfiniteScroll
-        CardComponent={TeamCard}
-        axiosInstance={teamApi.GET_TEAM_ARR}
-        clickLink={`${TEAM}/`}
-      />
+      <UserCardsGridWithInfiniteScroll CardComponent={TeamCard} clickLink={`${TEAM}/`} />
     </S.BoardWrapper>
   );
 }
