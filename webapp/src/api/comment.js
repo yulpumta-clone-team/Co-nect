66853a078be50811c6bf21bb295fb48150c12cd5
yunho <kimyouknow@naver.com>
2022-07-27 19:02:02 +0900

    Rename: api instance -> rootApiInstance

--- webapp/src/api/comment.js
+++ webapp/src/api/comment.js
@@ -1,61 +1,61 @@
 import { API } from 'constant/api';
-import instance from './core';
+import rootApiInstance from './core';
 
 const commentApi = {
   GET_COMMENT({ postType, postId }) {
-    return instance({
+    return rootApiInstance({
       url: `/${postType + API.COMMENT.ORIGIN}/${postId}`,
       method: 'get',
     });
   },
   POST_COMMENT({ postType, data }) {
-    return instance({
+    return rootApiInstance({
       url: `/${postType + API.COMMENT.ORIGIN}`,
       method: 'post',
       data,
     });
   },
   DELETE_COMMENT({ postType, id }) {
-    return instance({
+    return rootApiInstance({
       url: `/${postType + API.COMMENT.ORIGIN}/${id}`,
       method: 'delete',
     });
   },
   PATCH_COMMENT({ postType, id, data }) {
-    return instance({
+    return rootApiInstance({
       url: `/${postType + API.COMMENT.ORIGIN}/${id}`,
       method: 'patch',
       data,
     });
   },
   POST_REPLY({ postType, data }) {
-    return instance({
+    return rootApiInstance({
       url: `/${postType + API.COMMENT.NESTED}`,
       method: 'post',
       data,
     });
   },
   DELETE_REPLY({ postType, id }) {
-    return instance({
+    return rootApiInstance({
       url: `/${postType + API.COMMENT.NESTED}/${id}`,
       method: 'delete',
     });
   },
   PATCH_REPLY({ postType, id, data }) {
-    return instance({
+    return rootApiInstance({
       url: `/${postType + API.COMMENT.NESTED}/${id}`,
       method: 'patch',
       data,
     });
   },
   PATCH_COMMENT_LIKE({ postType, id }) {
-    return instance({
+    return rootApiInstance({
       url: `/${postType + API.COMMENT.LIKE}/${id}`,
       method: 'patch',
     });
   },
   PATCH_COMMENT_UN_LIKE({ postType, id }) {
-    return instance({
+    return rootApiInstance({
       url: `/${postType + API.COMMENT.UNLIKE}/${id}`,
       method: 'patch',
     });
