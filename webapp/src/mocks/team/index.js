886680599b7586c0f1c5e59e8eacde04ab2a9f4b
yunho <kimyouknow@naver.com>
2022-07-27 19:02:26 +0900

    Rename: msw에 적용 ROOT_URL -> ROOT_API_URL

--- webapp/src/mocks/team/index.js
+++ webapp/src/mocks/team/index.js
@@ -1,4 +1,4 @@
-import { API, ROOT_URL } from 'constant/api';
+import { API, ROOT_API_URL } from 'constant/api';
 import { getRandomStatusErrorCode, getResonseWithData } from 'mocks/mockUtils';
 import { rest } from 'msw';
 import { editTeamDetail } from './editTeamDetail';
@@ -7,13 +7,13 @@ import { teamsList } from './teamsList';
 
 const TEAM = [
   // GET_TEAM_ARR
-  rest.get(ROOT_URL + API.TEAM.LIST, (req, res, ctx) => {
+  rest.get(ROOT_API_URL + API.TEAM.LIST, (req, res, ctx) => {
     const lastPage = req.url.searchParams.get('lastPage');
     const newTeamList = teamsList.map((team) => ({ ...team, id: Number(team.id + lastPage) }));
     return res(ctx.status(200), ctx.delay(1500), ctx.json(getResonseWithData(newTeamList)));
   }),
   // GET_TEAM_LIKES
-  rest.get(`${ROOT_URL + API.TEAM.LIKES}`, (req, res, ctx) => {
+  rest.get(`${ROOT_API_URL + API.TEAM.LIKES}`, (req, res, ctx) => {
     const randomStatusErrorCode = getRandomStatusErrorCode();
     return res(
       ctx.status(randomStatusErrorCode),
@@ -22,7 +22,7 @@ const TEAM = [
     );
   }),
   // GET_TEAM_READS
-  rest.get(`${ROOT_URL + API.TEAM.READS}`, (req, res, ctx) => {
+  rest.get(`${ROOT_API_URL + API.TEAM.READS}`, (req, res, ctx) => {
     const randomStatusErrorCode = getRandomStatusErrorCode();
     return res(
       ctx.status(randomStatusErrorCode),
@@ -31,17 +31,17 @@ const TEAM = [
     );
   }),
   // GET_TEAM_DETAIL
-  rest.get(`${ROOT_URL + API.TEAM.DETAIL}/:id`, (req, res, ctx) => {
+  rest.get(`${ROOT_API_URL + API.TEAM.DETAIL}/:id`, (req, res, ctx) => {
     const randomStatusErrorCode = getRandomStatusErrorCode();
     return res(ctx.status(randomStatusErrorCode), ctx.json(getResonseWithData(teamDetail)));
   }),
   // POST_TEAM_POST
-  rest.post(`${ROOT_URL + API.TEAM.DETAIL}`, (req, res, ctx) => {
+  rest.post(`${ROOT_API_URL + API.TEAM.DETAIL}`, (req, res, ctx) => {
     console.log(req.body);
     return res(ctx.status(200), ctx.json(getResonseWithData(teamDetail)));
   }),
   // EDIT_TEAM_POST
-  rest.patch(`${ROOT_URL + API.TEAM.DETAIL}/:id`, (req, res, ctx) => {
+  rest.patch(`${ROOT_API_URL + API.TEAM.DETAIL}/:id`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(getResonseWithData(editTeamDetail)));
   }),
 ];
