a45177eb29d1df9f20d2efb3d3e29d2138884851
yunho <kimyouknow@naver.com>
2022-08-11 21:15:23 +0900

    Refactor: setAcessTokenInRequestConfig 분리

--- webapp/src/api/core/privateApiInstance.js
+++ webapp/src/api/core/privateApiInstance.js
@@ -11,15 +11,24 @@ const privateApiInstance = axios.create({
 privateApiInstance.defaults.timeout = 2500;
 // privateApiInstance.defaults.withCredentials = true;
 
+const setAcessTokenInRequestConfig = (config) => {
+  const accessToken = handleToken.getAccessToken();
+  const refreshToken = handleToken.getRefreshToken();
+  // 요청을 보내기 전에 수행할 로직
+
+  if (!config?.headers || !accessToken || !refreshToken) {
+    return config;
+  }
+  config.headers.Authorization = `Bearer ${accessToken}`;
+  config.headers[TOKEN.REFRESH] = refreshToken;
+  return config;
+};
+
 privateApiInstance.interceptors.request.use(
   (config) => {
-    const accessToken = handleToken.getAccessToken();
-    const refreshToken = handleToken.getRefreshToken();
-    // 요청을 보내기 전에 수행할 로직
     config.headers['Content-Type'] = 'application/json; charset=utf-8';
-    config.headers.Authorization = `Bearer ${accessToken}`;
-    config.headers[TOKEN.REFRESH] = refreshToken;
-    return config;
+    const newConfig = setAcessTokenInRequestConfig(config);
+    return newConfig;
   },
   (error) => {
     // 요청 에러가 발생했을 때 수행할 로직
