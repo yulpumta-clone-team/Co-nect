773049c72055159e317f4ef9c640e7b9ae883ae4
yunho <kimyouknow@naver.com>
2022-08-12 22:31:51 +0900

    Design: layout 기본 높이 설정
    gnb 높이만큼 padding-top

--- webapp/src/layouts/style.js
+++ webapp/src/layouts/style.js
@@ -6,12 +6,15 @@ export const AppContainer = styled.div`
 
 export const Main = styled.main`
   width: 100%;
+  height: 100vh;
   margin: 0 auto;
+  padding-top: 100px;
 `;
 
 export const Header = styled.header`
   width: 100%;
   height: 100px;
+  position: absolute;
   > div {
     width: 100vw;
     margin: 0 auto;
