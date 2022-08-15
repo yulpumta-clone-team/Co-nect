9786291f83ddb7af6268dcf52940de14d97010fd
yunho <kimyouknow@naver.com>
2022-08-15 18:54:44 +0900

    Feat: 회원가입페이지 useForm적용

--- webapp/src/pages/SignUp/style.js
+++ webapp/src/pages/SignUp/style.js
@@ -1,30 +1,51 @@
 import styled from 'styled-components';
 
-export const ModalContainer = styled.div`
+export const Container = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  position: fixed;
+  gap: 20px;
 `;
-export const DialogBox = styled.dialog`
-  width: 800px;
-  height: 400px;
+
+export const Header = styled.header`
   display: flex;
   flex-direction: column;
   align-items: center;
-  border: none;
-  border-radius: 3px;
-  box-shadow: 0 0 30px rgba(30, 30, 30, 0.185);
-  box-sizing: border-box;
-  background-color: white;
-  z-index: 10000;
-  top: 0;
+  justify-content: center;
+  gap: 30px;
+  > h2 {
+    font-family: 'Sandoll GothicNeo1';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 40px;
+    line-height: 28px;
+    /* identical to box height, or 70% */
+
+    /* Color/Primary/900 */
+    color: #036eff;
+  }
+  > span {
+    font-family: 'Sandoll GothicNeo1';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 32px;
+    /* identical to box height, or 133% */
+
+    /* Color/Gray/700 */
+    color: #818181;
+  }
 `;
-export const Backdrop = styled.div`
-  width: 100vw;
-  height: 200vh;
-  z-index: 9999;
-  background-color: rgba(0, 0, 0, 0.6);
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 18px;
 `;
+
+export const Backdrop = styled.div``;
