b7b34f6adc0595bd46328f6c82016d1152ee24ed
yunho <kimyouknow@naver.com>
2022-08-16 15:56:53 +0900

    Refactor: Divder컴포넌트에 margin props추가

--- webapp/src/components/Common/Divider/Divider.style.js
+++ webapp/src/components/Common/Divider/Divider.style.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const Container = styled.div`
   height: 2px;
@@ -6,4 +6,6 @@ export const Container = styled.div`
   background: ${({ theme: { colors } }) => colors.greyScale.nonActive};
   height: ${({ height }) => `${height}px`};
   width: ${({ width }) => `${width}px`};
+  margin-top: ${({ marginTop }) => `${marginTop}px`};
+  margin-bottom: ${({ marginBottom }) => `${marginBottom}px`};
 `;
