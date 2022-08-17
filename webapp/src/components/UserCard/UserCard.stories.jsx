1df4c4fd5cd1a01b896dcc2aa9f44d40fae56c6f
Wang HoEun <hoeunwang@gmail.com>
2022-08-17 15:18:23 +0900

    git develop 최신화를 위한 커밋

--- webapp/src/components/UserCard/UserCard.stories.jsx
+++ webapp/src/components/UserCard/UserCard.stories.jsx
@@ -26,8 +26,13 @@ export const Default = Template.bind({});
 Default.args = {
   cardInfo: {
     id: 0,
-    name: '스토리북 예시',
+    name: '홍길동',
+    userJob: '학생',
     hopeSession: '무관',
     likeCnt: 3,
+    img: 'https://user-images.githubusercontent.com/71386219/157435570-a48382a8-63e5-4d25-91f4-e506289424b5.png',
+    job: '학생',
+    belong_team: true,
+    skills: 1,
   },
 };
--- webapp/src/components/UserCard/style.js
+++ webapp/src/components/UserCard/style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const CardWrapper = styled.li`
   position: relative;
@@ -15,10 +15,54 @@ export const CardWrapper = styled.li`
 export const CardTop = styled.div`
   position: relative;
   display: flex;
-  height: 70px;
+  height: 50px;
   width: 100%;
   background: #036eff;
   border-radius: 10px 10px 0px 0px;
 `;
 
-export const Img = styled.div``;
+export const Image = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 15%;
+  height: 100%;
+  > img {
+    width: 40%;
+    height: 100%;
+  }
+`;
+export const BackgroundImg = styled.div`
+  position: relative;
+  align-items: flex-start;
+  display: flex;
+  width: 100%;
+  height: 20%;
+  background: #eaeaea;
+`;
+
+export const UserInfo = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-wrap: wrap;
+  > h2 {
+    font-family: 'Sandoll GothicNeo1';
+    font-style: normal;
+    font-weight: 800;
+    font-size: 25px;
+    line-height: 28px;
+  }
+`;
+
+export const Job = styled.button`
+  background-color: ${({ theme: { colors } }) => colors.secondary.normal};
+  padding: 5px 20px;
+  gap: 10px;
+
+  width: 80px;
+  height: 25px;
+  border-radius: 50px;
+`;
