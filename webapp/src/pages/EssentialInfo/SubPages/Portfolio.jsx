1b0e089fd4218adc837ed645f8fdf2a6acf7960c
yunho <kimyouknow@naver.com>
2022-08-21 17:24:18 +0900

    Feat: Portfolio useEssentialForm과 연결

--- webapp/src/pages/EssentialInfo/SubPages/Portfolio.jsx
+++ webapp/src/pages/EssentialInfo/SubPages/Portfolio.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import TextInput from 'components/Common/TextInput';
 import Button from 'components/Common/Button';
+import {
+  useEssentialFormsAction,
+  useEssentialFormsState,
+} from 'contexts/EssentialForm/EssentialForm.Provider';
 import * as S from '../EssentialInfo.style';
 
-Portfolio.propTypes = {};
-
-export default function Portfolio({}) {
+export default function Portfolio() {
+  const { inputValues } = useEssentialFormsState();
+  const { onChangeHandler, handleClickNextButton } = useEssentialFormsAction();
   return (
     <S.Content>
       <h2>포트폴리오를 업로드해주세요.</h2>
       <S.InputContainer>
         <TextInput
-          name="Portfolio"
+          name="portfolio"
           placeholder="포트폴리오(url)"
-          value=""
-          onChange={() => {}}
-          isError={false}
-          helperText=""
+          value={inputValues.portfolio}
+          onChange={onChangeHandler}
         />
       </S.InputContainer>
       <S.NextButtonContainer>
-        <Button theme="primary" type="submit" disabled={false} customStyle={S.NextButton}>
+        <Button theme="primary" customStyle={S.NextButton} onClick={handleClickNextButton}>
           다음
         </Button>
+        <span>포트폴리오 작업 없이 가입을 원할 경우 다음을 눌러주세요.</span>
       </S.NextButtonContainer>
     </S.Content>
   );
