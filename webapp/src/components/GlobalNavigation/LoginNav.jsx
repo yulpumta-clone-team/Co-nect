c6228099e72b71e69497ddee1f818d05f9919208
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2022-08-21 14:26:47 +0900

    [FE] GNB css (#144)
    * "Chore: svg파일경로 설정 오류 해결"
    
    * "Refectoring: css 작업 중 develop branch 최신화 를 위한 커밋"
    
    * "Refactoring: css 작업 도중 develop branch를 위한 commit"
    
    * Design: GNB 아직 수정중
    
    * Design: GNB css
    
    * Chore: 오류 해결을 위한 commit
    
    * Fix: GNB review 반영
    
    * Chore: commit 오류 수정
    
    * Fix: storybook 확인 후 부분 수정
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

--- webapp/src/components/GlobalNavigation/LoginNav.jsx
+++ webapp/src/components/GlobalNavigation/LoginNav.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { HOME, NEW_POST, TEAM, USER } from 'constant/route';
 import useDropdown from 'hooks/useDropdown';
 import { loggedUserType } from 'types/user.type';
+import Logo from 'assets/icons/nav-logo.svg';
+import Button from 'components/Common/Button';
 import UserInfoDropdown from './UserInfoDropdown';
 import * as S from './style';
 
@@ -15,25 +17,39 @@ export default function LoginNav({ userInfo }) {
   const { name, profileImg } = userInfo;
 
   return (
-    <S.LinkList>
-      <li>
-        <Link to={HOME}>Main</Link>
-      </li>
-      <li>
-        <Link to={USER}>User Board</Link>
-      </li>
-      <li>
-        <Link to={TEAM}>Team Board</Link>
-      </li>
-      <li ref={parent} onClick={openDropdown} style={{ position: 'relative' }}>
-        <img style={{ width: '30px' }} src={profileImg} alt="profile" />
-        <span>{name}</span>
-        <UserInfoDropdown
-          isDropdownOpen={isDropdownOpen}
-          shouldCloseDropdown={shouldCloseDropdown}
-          closeDropdown={closeDropdown}
-        />
-      </li>
-    </S.LinkList>
+    <S.Container>
+      <S.TopContainer />
+      <S.BoardList>
+        <S.Logo>
+          <img src={Logo} alt="conect-logo" />
+        </S.Logo>
+        <S.LinkList>
+          <S.Hover>
+            <Link to={HOME}>Home</Link>
+          </S.Hover>
+          <S.Hover>
+            <Link to={USER}>Single</Link>
+          </S.Hover>
+          <S.Hover>
+            <Link to={TEAM}>Team</Link>
+          </S.Hover>
+        </S.LinkList>
+        <S.AssignList isLogin>
+          <Button theme="primary" customStyle={{ width: 99, height: 35 }}>
+            <Link to={NEW_POST}>새글쓰기</Link>
+          </Button>
+          <S.Alarm />
+          <img src={profileImg} alt="profile" ref={parent} onClick={openDropdown} />
+          <S.UserName ref={parent} onClick={openDropdown}>
+            <span>{name}</span>
+          </S.UserName>
+          <UserInfoDropdown
+            isDropdownOpen={isDropdownOpen}
+            shouldCloseDropdown={shouldCloseDropdown}
+            closeDropdown={closeDropdown}
+          />
+        </S.AssignList>
+      </S.BoardList>
+    </S.Container>
   );
 }
