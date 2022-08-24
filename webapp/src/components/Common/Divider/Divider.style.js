926fecf2e4e5928daaacaa5c5f33a273ea705c3b
yunho <kimyouknow@naver.com>
2022-08-24 16:37:03 +0900

    Feat: Divider 세로 버전 추가

--- webapp/src/components/Common/Divider/Divider.style.js
+++ webapp/src/components/Common/Divider/Divider.style.js
@@ -1,8 +1,18 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const RowContainer = styled.div`
   height: 2px;
   width: 100%;
+  background: ${({ theme: { colors } }) => colors.greyScale.nonActive};
+  height: ${({ height }) => `${height}px`};
+  width: ${({ width }) => `${width}px`};
+  margin-top: ${({ marginTop }) => `${marginTop}px`};
+  margin-bottom: ${({ marginBottom }) => `${marginBottom}px`};
+`;
+
+export const ColContainer = styled.div`
+  height: 100%;
+  width: 2px;
   background: ${({ theme: { colors } }) => colors.greyScale.nonActive};
   height: ${({ height }) => `${height}px`};
   width: ${({ width }) => `${width}px`};
--- webapp/src/components/Common/Divider/index.jsx
+++ webapp/src/components/Common/Divider/index.jsx
@@ -4,14 +4,27 @@ import PropTypes from 'prop-types';
 import * as S from './Divider.style';
 
 Divider.propTypes = {
+  isRow: PropTypes.bool,
   width: PropTypes.string,
   height: PropTypes.string,
   marginTop: PropTypes.string,
   marginBottom: PropTypes.string,
 };
 
-export default function Divider({ width, height, marginTop, marginBottom }) {
-  return (
-    <S.Container width={width} height={height} marginTop={marginTop} marginBottom={marginBottom} />
+export default function Divider({ isRow = true, width, height, marginTop, marginBottom }) {
+  return isRow ? (
+    <S.RowContainer
+      width={width}
+      height={height}
+      marginTop={marginTop}
+      marginBottom={marginBottom}
+    />
+  ) : (
+    <S.ColContainer
+      width={width}
+      height={height}
+      marginTop={marginTop}
+      marginBottom={marginBottom}
+    />
   );
 }
