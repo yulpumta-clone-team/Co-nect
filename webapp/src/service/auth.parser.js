47cbd17672289737f8b5f175b838caf5590cc994
yunho <kimyouknow@naver.com>
2022-08-25 20:11:15 +0900

    Feat: authParser추가 - essentialInfoParser, skillStackParserToIds

--- webapp/src/service/auth.parser.js
+++ webapp/src/service/auth.parser.js
@@ -1,3 +1,5 @@
+import { skillStackParserToIds } from './skillStack.parser';
+
 export const signUpParser = (sigupRawData) => {
   const { email, password } = sigupRawData;
   return { email, pwd: password };
@@ -6,4 +8,29 @@ export const signUpParser = (sigupRawData) => {
 export const loginParser = (loginRawData) => {
   const { email, password } = loginRawData;
   return { email, pwd: password };
+};
+
+export const essentialInfoParser = (essentialInfoRawData) => {
+  const {
+    introduction,
+    hopeSession,
+    profileImage,
+    job,
+    nickname,
+    portfolio,
+    slogan,
+    techSkills,
+    belongTeam,
+  } = essentialInfoRawData;
+  const paresedTechSkills = skillStackParserToIds(techSkills);
+  return {
+    content: introduction,
+    hope_session: hopeSession,
+    image: profileImage,
+    job,
+    name: nickname,
+    portfolio,
+    slogan,
+    skilss: paresedTechSkills,
+  };
 };
