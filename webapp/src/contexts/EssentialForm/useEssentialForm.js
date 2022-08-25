d42c0bda749b497a7335d2170cfd9ba6cd0d9e8d
yunho <kimyouknow@naver.com>
2022-08-25 20:05:44 +0900

    Feat: 유저필수정보입력 시 닉네임 중복 관련 핸들링

--- webapp/src/contexts/EssentialForm/useEssentialForm.js
+++ webapp/src/contexts/EssentialForm/useEssentialForm.js
@@ -6,8 +6,9 @@ import { notifyNewMessage } from 'contexts/ToastNotification/action';
 import useForm from 'hooks/useForm';
 import authApi from 'api/auth.api';
 import userApi from 'api/user.api';
-import { ESSENTIAL_INFO } from 'constant/route.constant';
+import { ESSENTIAL_INFO, LOGIN } from 'constant/route.constant';
 import essentialValidation from 'service/essentialForm.validation';
+import { essentialInfoParser } from 'service/auth.parser';
 
 const initialValues = {
   nickname: '',
@@ -40,13 +41,12 @@ const useEssentialForm = () => {
   const [isNicknameDuplicate, setIsNicknameDuplicate] = useState(true);
 
   const submitCallback = async (submitData) => {
+    const parsedSubmitData = essentialInfoParser(submitData);
     // TODO: 1초가 넘으면 처리중입니다 메세지 보여지게 수정
     notifyNewMessage(notifyDispatch, '처리 중입니다...', TOAST_TYPE.Info);
 
-    console.log('submitData', submitData);
-
     try {
-      const response = await userApi.POST_ESSENTIAL_INFO(submitData);
+      const response = await userApi.POST_ESSENTIAL_INFO({ submitData: parsedSubmitData });
       const { message } = response.data;
       notifyNewMessage(notifyDispatch, message, TOAST_TYPE.Success);
       setTimeout(() => {
@@ -84,7 +84,10 @@ const useEssentialForm = () => {
     const currentPathname = location.pathname;
     const currentSubPageIndex = essentailSubPagesRouteOrder.indexOf(currentPathname);
     // 타켓팅한 요소의 인덱스가 배열의 길이보다 작거나 같을 때는 이동하지 않는다.
-    if (currentSubPageIndex <= 0) return;
+    if (currentSubPageIndex <= 0) {
+      navigate(LOGIN);
+      return;
+    }
     navigate(essentailSubPagesRouteOrder[currentSubPageIndex - 1]);
   }, [location.pathname, navigate]);
 
@@ -92,16 +95,21 @@ const useEssentialForm = () => {
     // TODO: 1초가 넘으면 처리중입니다 메세지 보여지게 수정
     notifyNewMessage(notifyDispatch, '처리 중입니다...', 'Info');
     try {
-      const response = await authApi.checkDuplicateNickName({ email: inputValues.email });
-      const { message } = response.data;
-      notifyNewMessage(notifyDispatch, message, 'Success');
-      setIsNicknameDuplicate(false);
+      const response = await authApi.checkDuplicateNickName({ name: inputValues.nickname });
+      const isDuplicated = response.data;
+      if (isDuplicated) {
+        notifyNewMessage(notifyDispatch, '이미 사용중인 닉네임입니다!', TOAST_TYPE.Warning);
+        setIsNicknameDuplicate(true);
+      } else {
+        notifyNewMessage(notifyDispatch, '사용가능한 닉네임입니다!', TOAST_TYPE.Success);
+        setIsNicknameDuplicate(false);
+      }
     } catch (error) {
       console.error(error);
       notifyNewMessage(notifyDispatch, error, 'Error');
       setIsNicknameDuplicate(true);
     }
-  }, [inputValues.email, notifyDispatch]);
+  }, [inputValues.nickname, notifyDispatch]);
 
   // try {
   //   const response = await authApi.GET_ESSENTIAL_INFO();
