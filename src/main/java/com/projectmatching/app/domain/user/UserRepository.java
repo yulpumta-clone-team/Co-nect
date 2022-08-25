9c70d3315d7de2e94a0927d3127f71fdbd5e62a3
philos1234 <ohhyun1948@gmail.com>
2022-08-25 20:07:53 +0900

    fix : swagger refresh key 받도록 추가

--- src/main/java/com/projectmatching/app/domain/user/UserRepository.java
+++ src/main/java/com/projectmatching/app/domain/user/UserRepository.java
@@ -2,6 +2,7 @@ package com.projectmatching.app.domain.user;
 
 import com.projectmatching.app.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,7 +15,6 @@ public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByName(String name);
-
 
 
     boolean existsByEmail(String email);
