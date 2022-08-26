8c6fbf6767d6965ca0feb87697b2cbf1c8c47ba8
yunho <kimyouknow@naver.com>
2022-08-26 19:55:16 +0900

    Feat: Image컴포넌트에 fowardRef적용

--- webapp/src/components/Common/Image/index.jsx
+++ webapp/src/components/Common/Image/index.jsx
@@ -1,18 +1,20 @@
 import { DEFAULT_PROFILE_IMG } from 'constant';
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 
 import * as S from './style';
+
+const Image = forwardRef(({ src, customStyle }, ref) => {
+  const handleError = (event) => {
+    event.target.onerror = null;
+    event.target.src = DEFAULT_PROFILE_IMG;
+  };
+  return <S.Image ref={ref} src={src} onError={handleError} customStyle={customStyle} />;
+});
 
 Image.propTypes = {
   src: PropTypes.string.isRequired,
   customStyle: PropTypes.array,
 };
 
-export default function Image({ src, customStyle }) {
-  const handleError = (event) => {
-    event.target.onerror = null;
-    event.target.src = DEFAULT_PROFILE_IMG;
-  };
-  return <S.Image src={src} onError={handleError} customStyle={customStyle} />;
-}
+export default Image;
