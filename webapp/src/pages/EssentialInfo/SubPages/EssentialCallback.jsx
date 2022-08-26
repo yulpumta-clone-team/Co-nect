2ae450ec5f4090dff6d940d407771aa14e3b3979
yunho <kimyouknow@naver.com>
2022-08-26 19:54:54 +0900

    Chore: EssentailCallback 주석작성
    // TODO: 특정시간동안 반응없으면 essential-info로 이동시키기

--- webapp/src/pages/EssentialInfo/SubPages/EssentialCallback.jsx
+++ webapp/src/pages/EssentialInfo/SubPages/EssentialCallback.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Loader from 'components/Common/Loader';
+
 import * as S from '../EssentialInfo.style';
 
+// TODO: 특정시간동안 반응없으면 essential-info로 이동시키기
 export default function EssentailCallback() {
   return (
     <S.Content>
