1d67b8113582713d0c35d7b7c03b7403092654a1
yunho <kimyouknow@naver.com>
2022-08-28 22:13:37 +0900

    Fix: 이미지 컴포넌트

--- webapp/src/components/Common/Image/index.jsx
+++ webapp/src/components/Common/Image/index.jsx
@@ -4,16 +4,17 @@ import MainLogoIcon from 'assets/icons/conect-main.svg';
 
 import * as S from './style';
 
-const Image = forwardRef(({ src, customStyle }, ref) => {
+const Image = forwardRef(({ src = '', alt, customStyle }, ref) => {
   const handleError = (event) => {
     event.target.onerror = null;
     event.target.src = MainLogoIcon;
   };
-  return <S.Image ref={ref} src={src} onError={handleError} customStyle={customStyle} />;
+  return <S.Image ref={ref} src={src} alt={alt} onError={handleError} customStyle={customStyle} />;
 });
 
 Image.propTypes = {
-  src: PropTypes.string.isRequired,
+  src: PropTypes.string,
+  alt: PropTypes.string.isRequired,
   customStyle: PropTypes.array,
 };
 
--- webapp/src/components/Common/Image/style.js
+++ webapp/src/components/Common/Image/style.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Image = styled.img`
-  width: 30%;
-  height: 25%;
+  width: 100%;
+  height: 100%;
   border-radius: 50%;
 
   ${({ customStyle }) => customStyle}
