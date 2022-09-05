32c0cab67f3c42464a4fd93edbc3ac954cee6bdc
Wang HoEun <hoeunwang@gmail.com>
2022-09-06 00:07:09 +0900

    CSS: TeamPostDetail

--- webapp/src/pages/TeamPost/TeamPost.style.js
+++ webapp/src/pages/TeamPost/TeamPost.style.js
@@ -1,4 +1,7 @@
 import styled, { css } from 'styled-components';
+import { ReactComponent as blueHeartIcon } from 'assets/icons/blue-heart.svg';
+import { ReactComponent as chatIcon } from 'assets/icons/chat.svg';
+import { ReactComponent as viewIcon } from 'assets/icons/view.svg';
 
 export const Container = styled.div`
   position: relative;
@@ -9,8 +12,8 @@ export const Container = styled.div`
 export const PostContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  width: 1100px;
-  height: 1250px;
+  width: 1250px;
+  height: 1300px;
   background: #ffffff;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
@@ -22,7 +25,7 @@ export const ImgContainer = styled.div`
   align-items: center;
   flex-direction: column;
   width: 100%;
-  height: 144px;
+  height: 12%;
   border-radius: 20px 20px 0px 0px;
   background-color: ${({ theme: { colors } }) => colors.greyScale.background};
 `;
@@ -43,7 +46,6 @@ export const TeamInfoContainer = styled.div`
 `;
 
 export const CommentDivider = css`
-  width: 95%;
   background-color: ${({ theme: { colors } }) => colors.primary.normal};
 `;
 
@@ -51,16 +53,94 @@ export const SingleInfo = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
-  gap: 24px;
+  gap: 2%;
   margin-bottom: 20px;
   > span {
     width: 30%;
   }
   > div {
-    width: 70%;
+    width: 87%;
   }
 `;
 export const SingleInfoTitle = styled.span`
   color: ${({ theme: { colors } }) => colors.primary.normal};
   ${({ theme: { fonts } }) => fonts.korean.emphasis};
+  ${({ customStyle }) => customStyle}
+`;
+export const UserName = styled.div`
+  position: absolute;
+  top: 100px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  ${({ theme: { fonts } }) => fonts.korean.emphasis};
+  font-weight: 700;
+`;
+export const Skill = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  > span {
+    width: 10%;
+    ${({ theme: { fonts } }) => fonts.korean.emphasis};
+    font-weight: 700;
+  }
+  > div {
+    width: 70%;
+  }
+`;
+export const HopeSession = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin-bottom: 20px;
+  > span {
+    width: 10%;
+    ${({ theme: { fonts } }) => fonts.korean.emphasis};
+    font-weight: 700;
+  }
+  > div {
+    width: 70%;
+  }
+`;
+export const EmptyImage = styled.div`
+  background-color: ${({ theme: { colors } }) => colors.greyScale.background};
+  width: 100%;
+  height: 100%;
+  border-radius: 20px 20px 0px 0px;
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+`;
+export const UserImg = css`
+  position: relative;
+  width: 3%;
+  height: 100%;
+`;
+export const TeamName = css`
+  ${({ theme: { fonts } }) => fonts.korean.title};
+  color: #3c3c3c;
+`;
+export const Slogan = css`
+  ${({ theme: { fonts } }) => fonts.korean.subTitle};
+  font-weight: 700;
+  color: #3c3c3c;
+`;
+
+export const TeamSkillSession = css`
+  position: relative;
+  width: 3%;
+`;
+export const Heart = styled(blueHeartIcon)`
+  color: ${({ theme: { colors } }) => colors.primary.normal};
+  width: 25px;
+  height: 25px;
+`;
+export const Chat = styled(chatIcon)`
+  width: 25px;
+  height: 25px;
+`;
+export const View = styled(viewIcon)`
+  width: 25px;
+  height: 25px;
 `;
--- webapp/src/pages/TeamPost/TeamPostDetail.jsx
+++ webapp/src/pages/TeamPost/TeamPostDetail.jsx
@@ -6,6 +6,8 @@ import { POST_TYPE } from 'constant';
 import Divider from 'components/Common/Divider';
 import { teamDetailParser } from 'service/team.parser';
 import TechSkills from 'components/TechSkills';
+import Image from 'components/Common/Image';
+import Slogan from 'pages/EssentialInfo/SubPages/Slogan';
 import * as S from './TeamPost.style';
 
 const javascriptUrl =
@@ -24,45 +26,52 @@ export const skillsImg = [
 
 export default function TeamPostDetail({ targetTeam }) {
   const parsedTargetTeam = teamDetailParser(targetTeam);
-  const { id, name, user, content, img, hopeSession, skills, commentCnt, likeCnt } =
+  const { id, name, user, content, img, hopeSession, skills, slogan, commentCnt, likeCnt } =
     parsedTargetTeam;
 
   // const { id, name, content, session, img, read, skills, commentCnt, likeCnt, user } = targetTeam;
   return (
     <S.PostContainer>
       <S.ImgContainer>
-        <S.ViewingImage src={img} alt="게시글" />
+        {img ? <S.ViewingImage src={img} alt="게시글" /> : <S.EmptyImage />}
       </S.ImgContainer>
       <S.TeamInfoContainer>
+        <S.UserName>
+          <Image src={img} alt="프로필 이미지" customStyle={S.UserImg} />
+          {user.name}
+        </S.UserName>
         <S.SingleInfo>
-          <S.SingleInfoTitle>사용자 명</S.SingleInfoTitle>
-          <div>
-            <span>{user.name}</span>
-          </div>
+          <S.SingleInfoTitle customStyle={S.TeamName}>{name}</S.SingleInfoTitle>
         </S.SingleInfo>
-        <S.SingleInfo>
-          <S.SingleInfoTitle>팀 이름</S.SingleInfoTitle>
-          <div>
-            <span>{name}</span>
-          </div>
-        </S.SingleInfo>
-        <S.SingleInfo>
-          <S.SingleInfoTitle>기술 스택</S.SingleInfoTitle>
-          <div>
-            <TechSkills skills={skills} imageSize="30px" />
-          </div>
-        </S.SingleInfo>
-        <S.SingleInfo>
-          <S.SingleInfoTitle> 희망 작업 기간</S.SingleInfoTitle>
+        <Divider />
+        <S.Skill>
+          <S.SingleInfoTitle customStyle={S.TeamSkillSession}>기술 스택</S.SingleInfoTitle>
+          <TechSkills skills={skills} imageSize="40px" />
+        </S.Skill>
+        <S.HopeSession>
+          <S.SingleInfoTitle customStyle={S.TeamSkillSession}> 희망 작업 기간</S.SingleInfoTitle>
           <div>
             <span>{hopeSession}</span>
           </div>
+        </S.HopeSession>
+
+        <Divider />
+        <S.SingleInfo>
+          <S.SingleInfoTitle customStyle={S.Slogan}>{slogan}</S.SingleInfoTitle>
         </S.SingleInfo>
         <S.SingleInfo>
-          <S.SingleInfoTitle>자기소개</S.SingleInfoTitle>
           <MarkdownEditor onlyViewer content={content} />
         </S.SingleInfo>
         <Divider customStyle={S.CommentDivider} />
+        <S.SingleInfo>
+          <div>
+            <S.View />
+          </div>
+          <S.Heart />
+          {likeCnt}
+          <S.Chat />
+          {commentCnt}
+        </S.SingleInfo>
         <CommentContainer postType={POST_TYPE.TEAM} postWriter={user.name} postId={id} />
       </S.TeamInfoContainer>
     </S.PostContainer>
