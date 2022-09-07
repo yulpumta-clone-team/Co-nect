8784734fd1c6541df0c2356df7396cd8286f3406
yunho <kimyouknow@naver.com>
2022-09-07 23:01:46 +0900

    Story: WtihSelectedOptionViewer 스토리 추가

--- webapp/src/components/TechStackSelectInput/TechStackSelectInput.stories.jsx
+++ webapp/src/components/TechStackSelectInput/TechStackSelectInput.stories.jsx
@@ -1,11 +1,10 @@
 import handlers from 'mocks/handlers';
-import React from 'react';
+import React, { useState } from 'react';
 import TechStackSelectInput from './index';
 
 export default {
   title: 'Component/TechStackSelectInput',
   component: TechStackSelectInput,
-  args: {},
   parameters: {
     msw: handlers,
   },
@@ -13,7 +12,13 @@ export default {
 };
 
 function Template(args) {
-  return <TechStackSelectInput {...args} />;
+  const [selectedTechSkills, setSelectedTechSkills] = useState([]);
+  const onChange = ({ name, value }) => {
+    setSelectedTechSkills(value);
+  };
+  return (
+    <TechStackSelectInput {...args} selectedTechSkills={selectedTechSkills} onChange={onChange} />
+  );
 }
 
 export const Default = Template.bind({});
@@ -26,6 +31,24 @@ Default.args = {
 
 export const WtihValue = Template.bind({});
 WtihValue.args = {
+  selectedTechSkills: [
+    {
+      id: 500,
+      category: 'arichitecture',
+      label: 'git',
+      value: 'git',
+      image:
+        'https://user-images.githubusercontent.com/71386219/186051220-a77fa08e-b501-4baa-af3c-47ae602d25e1.png',
+    },
+  ],
+  name: 'techSkills',
+  label: '기술스택선택하기',
+  placeholder: 'selectedTechSkills',
+};
+
+export const WtihSelectedOptionViewer = Template.bind({});
+WtihSelectedOptionViewer.args = {
+  showSelectedOption: true,
   selectedTechSkills: [
     {
       id: 500,
