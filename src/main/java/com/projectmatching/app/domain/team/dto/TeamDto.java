bc11ed19a00c7ba4c03b19f40702e2f634196595
philos1234 <ohhyun1948@gmail.com>
2022-09-14 23:49:59 +0900

    fix : Team 상세조회 순환 참조 문제 해결

--- src/main/java/com/projectmatching/app/domain/team/dto/TeamDto.java
+++ src/main/java/com/projectmatching/app/domain/team/dto/TeamDto.java
@@ -1,20 +1,21 @@
 package com.projectmatching.app.domain.team.dto;
 
+import com.projectmatching.app.domain.comment.dto.TeamCommentDto;
 import com.projectmatching.app.domain.comment.entity.TeamComment;
+import com.projectmatching.app.domain.liking.dto.TeamLikingDto;
+import com.projectmatching.app.domain.liking.dto.UserLikingDto;
 import com.projectmatching.app.domain.liking.entity.TeamLiking;
 import com.projectmatching.app.domain.team.entity.Team;
 import com.projectmatching.app.domain.team.entity.TeamTech;
 import com.projectmatching.app.domain.user.dto.UserInfo;
+import com.projectmatching.app.domain.user.dto.users.UserTeamDto;
 import com.projectmatching.app.domain.user.entity.UserTeam;
-import com.projectmatching.app.util.StreamUtil;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.BeanUtils;
 
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 
@@ -31,27 +32,30 @@ public class TeamDto {
     private String session;
 
     private String content;
-
-
     private Long readCnt;
-    @ApiModelProperty(name = "해당 팀에 속한 유저들")
-    private List<UserTeam> userTeamList;
+    private List<UserTeamDto> userTeamList;
 
+    private List<TeamCommentDto> comments;
 
-    private List<TeamComment> comments;
-    @ApiModelProperty(name = "해당 팀을 좋아요함 유저정보")
-    private List<TeamLiking> teamLikings;
+    private List<TeamLikingDto> teamLikings;
 
-    @ApiModelProperty(name = "해당 팀의 기술 스택들")
-    private List<TeamTech> teamTeches;
+    private List<TeamTechDto> teamTeches;
 
     public static TeamDto of(Team team){
         TeamDto teamDto = new TeamDto();
         BeanUtils.copyProperties(team,teamDto);
-        teamDto.comments = team.getTeamComments().stream().collect(Collectors.toList());
-        teamDto.userTeamList = team.getUserTeams().stream().collect(Collectors.toList());
-        teamDto.teamLikings = team.getTeamLikings().stream().collect(Collectors.toList());
-        teamDto.teamTeches = team.getTeamTeches().stream().collect(Collectors.toList());
+        teamDto.comments = team.getTeamComments().stream().map(TeamCommentDto::of)
+                .collect(Collectors.toList());
+
+        teamDto.userTeamList = team.getUserTeams().stream()
+                .map(UserTeamDto::forTeamOf).collect(Collectors.toList());
+
+        teamDto.teamLikings = team.getTeamLikings().stream()
+                .map(TeamLikingDto::of)
+                .collect(Collectors.toList());
+        teamDto.teamTeches = team.getTeamTeches().stream()
+                .map(TeamTechDto::of )
+                .collect(Collectors.toList());
 
         return teamDto;
     }
