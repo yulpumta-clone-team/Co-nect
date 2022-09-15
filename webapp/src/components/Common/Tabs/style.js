8a11a18dcfc522b1c79c71ed1fb426c95f955336
Wang HoEun <hoeunwang@gmail.com>
2022-09-15 22:23:29 +0900

    Css: MyList css

--- webapp/src/components/Common/Tabs/style.js
+++ webapp/src/components/Common/Tabs/style.js
@@ -4,10 +4,22 @@ export const Tabs = styled.ul`
   width: 100%;
   display: flex;
   justify-content: center;
-  gap: 12px;
+  align-items: flex-end;
+  gap: 50px;
 `;
-
+// (isActive ? 'white' : '#036EFF')
 export const Tab = styled.li`
-  border-bottom: ${({ isActive }) => isActive && '5px solid black'};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  height: 30px;
+  border: 2px solid ${({ theme: { colors } }) => colors.primary.normal};
+  border-radius: 5px;
+  background-color: ${({ isActive }) => isActive && '#036EFF'};
+  color: ${({ isActive }) => (isActive ? 'white' : '#036EFF')};
+  ${({ theme: { fonts } }) => fonts.korean.title};
+  font-size: 16px;
+
   cursor: pointer;
 `;
--- webapp/src/pages/MyList/style.js
+++ webapp/src/pages/MyList/style.js
@@ -1,5 +1,19 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 1500px;
+`;
+export const BoardContainer = styled.div`
+  width: 1180px;
+  height: 85vh;
+  margin: 0 auto;
+`;
 
-export const SessionContainer = styled.div``;
+export const SessionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 1500px;
+  height: 10vh;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+  margin: 0px 0px 30px 0px;
+`;
