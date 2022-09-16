5e0a0327882744d952ee98fe4431a5e9dca22498
Wang HoEun <hoeunwang@gmail.com>
2022-09-16 21:01:51 +0900

    Fix: review 반영하여 height 관련 값 수정

--- webapp/src/pages/MyList/style.js
+++ webapp/src/pages/MyList/style.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  width: 1500px;
-`;
+export const Container = styled.div``;
 export const BoardContainer = styled.div`
   width: 1180px;
   height: 85vh;
@@ -13,7 +11,7 @@ export const SessionContainer = styled.div`
   display: flex;
   align-items: center;
   width: 1500px;
-  height: 10vh;
+  height: 70px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
   margin: 0px 0px 30px 0px;
 `;
