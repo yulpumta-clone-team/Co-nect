760eadfa0762d574bda57d1cd052f0e7299b8fc1
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2022-09-18 23:19:41 +0900

    [FE] MyList - css (#181)
    * Css: MyList css
    
    * Fix: color theme 활용
    
    * Fix: review 반영하여 height 관련 값 수정
    
    * Design: layout스타일 수정 (height, padding-top)
    
    Header 기본 높이 제거 (gnb에서 설정해놔서 여기서 설정할 필요가 없다.)
    
    * Design: 유저보드,팀보드 기본 높이 제거
    
    * Story: MyList스토리 작성
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>
    Co-authored-by: yunho <kimyouknow@naver.com>

--- webapp/src/components/Common/Tabs/style.js
+++ webapp/src/components/Common/Tabs/style.js
@@ -4,10 +4,23 @@ export const Tabs = styled.ul`
   width: 100%;
   display: flex;
   justify-content: center;
-  gap: 12px;
+  align-items: flex-end;
+  gap: 50px;
 `;
-
 export const Tab = styled.li`
-  border-bottom: ${({ isActive }) => isActive && '5px solid black'};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  height: 30px;
+  border: 2px solid ${({ theme: { colors } }) => colors.primary.normal};
+  border-radius: 5px;
+  background-color: ${({ isActive }) =>
+    isActive ? ({ theme: { colors } }) => colors.primary.normal : 'white'};
+  color: ${({ isActive }) =>
+    isActive ? 'white' : ({ theme: { colors } }) => colors.primary.normal};
+  ${({ theme: { fonts } }) => fonts.korean.title};
+  font-size: 16px;
+
   cursor: pointer;
 `;
