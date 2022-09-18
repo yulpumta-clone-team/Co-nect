f7f5aff09337d8fd1ad28010d948891335f1c384
yunho <kimyouknow@naver.com>
2022-09-18 16:52:58 +0900

    Feat: EditTeamPost view와 detail분리

--- webapp/src/pages/EditTeamPost/EditTeamPost.style.js
+++ webapp/src/pages/EditTeamPost/EditTeamPost.style.js
@@ -1,13 +1,104 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { ReactComponent as addImgIcon } from 'assets/icons/add-img.svg';
+import { ReactComponent as editIcon } from 'assets/icons/edit.svg';
+
+export const ImgContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 144px;
+  border-radius: 20px 20px 0px 0px;
+  background-color: ${({ theme: { colors } }) => colors.greyScale.background};
+`;
+export const ViewingImage = styled.img`
+  background-color: ${({ theme: { colors } }) => colors.greyScale.background};
+  width: 100%;
+  height: 100%;
+  border-radius: 20px 20px 0px 0px;
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+`;
+
+// 아직 NewTeamPost에 대한 editbutton css가 나오지 않아, 임의로 위치 지정함.
+export const ImageEditButton = styled(editIcon)`
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  left: 63%;
+  top: 10%;
+`;
+export const InputTypeImageHandler = styled.label`
+  background-color: ${({ theme: { colors } }) => colors.greyScale.background};
+  width: 100%;
+  height: 100%;
+  border-radius: 20px 20px 0px 0px;
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  > div {
+    background-color: ${({ theme: { colors } }) => colors.primary.normal};
+    width: 52px;
+    height: 52px;
+    border-radius: 50%;
+    ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  }
+  &:hover {
+    cursor: pointer;
+  }
+`;
+export const HiddenInputHandler = styled.input`
+  display: none;
+`;
+export const AddImage = styled(addImgIcon)`
+  width: 50px;
+  height: 50px;
+`;
 
 export const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+export const PostContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 600px;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  width: 800px;
+  justify-content: space-between;
   margin: 0 auto;
-  padding: 15px;
-  border: 3px solid #ffdf65;
-  border-radius: 15px;
-  box-shadow: 2px 2px 2px 2px gray;
-  flex-wrap: wrap;
+`;
+export const InfoContainer = styled.div`
+  width: 650px;
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  border: 3px solid ${({ theme: { colors } }) => colors.primary.normal};
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+`;
+export const ButtonContainer = styled.div`
+  width: 500px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: flex-end;
+`;
+export const SubmitButton = css`
+  width: 270px;
+  height: 56px;
+  margin-top: 19px;
+  ${({ theme: { fonts } }) => fonts.korean.title};
+  font-size: 1.6rem; // size 따로 지정해준건, 해당 theme이 없었기 때문입니다.
+`;
+export const PostInfoContainer = styled.div`
+  padding: 20px 55px 50px 55px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
 `;
--- webapp/src/pages/EditTeamPost/EditTeamPostDetail.jsx
+++ webapp/src/pages/EditTeamPost/EditTeamPostDetail.jsx
@@ -1,11 +1,88 @@
+import teamApi from 'api/team.api';
+import { useToastNotificationAction } from 'contexts/ToastNotification';
+import { notifyNewMessage } from 'contexts/ToastNotification/action';
+import { TOAST_TYPE } from 'contexts/ToastNotification/type';
+import useAxios from 'hooks/useAxios';
+import useFileUploader from 'hooks/useFileUploader';
+import useForm from 'hooks/useForm';
 import React from 'react';
+import { teamDetailParser } from 'service/team/team.parser';
+import { newTeamPostValidation } from 'service/team/team.validation';
 import { teamDetailType } from 'types/team.type';
+import EditTeamPostView from './EditTeamPost.view';
 
 EditTeamPostDetail.propTypes = {
   targetTeam: teamDetailType,
 };
 
 export default function EditTeamPostDetail({ targetTeam }) {
-  console.log('targetTeam', targetTeam);
-  return <div>EditTeamPostDetail</div>;
+  const notifyDispatch = useToastNotificationAction();
+
+  const { teamId, teamName, teamImage, techSkills, hopeSession, content, slogan, writerInfo } =
+    teamDetailParser(targetTeam);
+
+  // 수정 요청 api hooks
+  const [state, execution, foreceRefetch] = useAxios({
+    axiosInstance: teamApi.EDIT_TEAM_POST,
+    immediate: false,
+  });
+
+  // s3 이미지 업로더 api hooks
+  const { uploadFileOnS3, imageFile, onChangeFile } = useFileUploader();
+
+  const uploadImageFileBeforeSubmit = async (submitData) => {
+    const response = await uploadFileOnS3();
+    if (response) {
+      const { id, path } = response;
+      return { ...submitData, profileImage: path };
+    }
+    return submitData;
+  };
+
+  // 수정 요청
+  const submitCallback = async (submitData) => {
+    const changedProfileImageSubmitData = await uploadImageFileBeforeSubmit(submitData);
+    console.log('changedProfileImageSubmitData', changedProfileImageSubmitData);
+    // const parsedSubmitData = userPostEditParser(changedProfileImageSubmitData);
+    // await execution({ data: parsedSubmitData });
+    // TODO: 성공시 이동할 페이지 정해서 이동시키기
+    notifyNewMessage(notifyDispatch, '수정 완료!', TOAST_TYPE.Success);
+  };
+
+  const {
+    inputValues,
+    validateError,
+    onChangeHandler,
+    onChangeHandlerWithSelect,
+    submitHandler,
+    satisfyAllValidates,
+    isTargetSatisfyValidate,
+  } = useForm({
+    initialValues: {
+      teamName,
+      teamImage,
+      techSkills,
+      slogan,
+      hopeSession,
+      content,
+    },
+    submitCallback,
+    validate: newTeamPostValidation,
+  });
+
+  const profileImageSrc = inputValues.teamImage || (imageFile && URL.createObjectURL(imageFile));
+
+  return (
+    <EditTeamPostView
+      inputValues={inputValues}
+      onChangeHandler={onChangeHandler}
+      onChangeHandlerWithSelect={onChangeHandlerWithSelect}
+      submitHandler={submitHandler}
+      validateError={validateError}
+      satisfyAllValidates={satisfyAllValidates}
+      isTargetSatisfyValidate={isTargetSatisfyValidate}
+      profileImageSrc={profileImageSrc}
+      onChangeFile={onChangeFile}
+    />
+  );
 }
