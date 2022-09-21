a10e7726bf033fa9f477c1ad9ad360dea7a06772
yunho <kimyouknow@naver.com>
2022-09-21 21:04:15 +0900

    Story: RootCommentElement params 수정

--- webapp/src/components/ComentContainer/CommentElement/Root.CommentElement.stories.jsx
+++ webapp/src/components/ComentContainer/CommentElement/Root.CommentElement.stories.jsx
@@ -1,5 +1,5 @@
-import CommentProvider from 'contexts/Comment/Comment.Provider';
 import React from 'react';
+import CommentProvider from 'contexts/Comment/Comment.Provider';
 import RootCommentElement from './Root.CommentElement';
 
 export default {
@@ -24,7 +24,22 @@ Default.args = {
     img: '',
     writer: '수정된',
     parentId: null,
-    secret: true,
+    secret: false,
+    content: '수정된 연습용 댓긇',
+    feeling: [901092146501, 92719182999999, 927191821029312],
+  },
+  postWriter: 'front_temp',
+  replies: [],
+};
+
+export const WithReplies = Template.bind({});
+WithReplies.args = {
+  commentInfo: {
+    id: 999,
+    img: '',
+    writer: '수정된',
+    parentId: null,
+    secret: false,
     content: '수정된 연습용 댓긇',
     feeling: [901092146501, 92719182999999, 927191821029312],
   },
@@ -36,7 +51,7 @@ Default.args = {
       userId: 12512512,
       writer: 'front_temp',
       parentId: 999,
-      secret: true,
+      secret: false,
       content: '연습용',
       feeling: [901092146501],
     },
@@ -46,7 +61,7 @@ Default.args = {
       userId: 12512512,
       writer: 'string',
       parentId: 999,
-      secret: true,
+      secret: false,
       content: '수정된',
       feeling: [784833333333, 19148710],
     },
