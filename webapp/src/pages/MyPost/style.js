ebb2e5c7be7a32f0f3efd5be1e66cd73b11e2f08
Wang HoEun <hoeunwang@gmail.com>
2022-09-21 20:35:36 +0900

    Fix: myList와 같은 BoardContainer로 수정

--- webapp/src/pages/MyPost/style.js
+++ webapp/src/pages/MyPost/style.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div``;
 export const BoardContainer = styled.div`
   width: 1180px;
-  height: 85vh;
   margin: 0 auto;
+  padding: 30px;
 `;
