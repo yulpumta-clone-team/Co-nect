2c5f807806b74d01544793a03bf573dc0ded9970
philos1234 <48795102+philos1234@users.noreply.github.com>
2022-09-24 13:02:29 +0900

    #192/fix : 팀 조회 skill dto 수정

--- src/main/java/com/projectmatching/app/domain/team/dto/TeamSimpleDto.java
+++ src/main/java/com/projectmatching/app/domain/team/dto/TeamSimpleDto.java
@@ -2,6 +2,7 @@ package com.projectmatching.app.domain.team.dto;
 
 
 import com.projectmatching.app.domain.team.entity.Team;
+import com.projectmatching.app.domain.techStack.dto.TechStackDto;
 import com.projectmatching.app.domain.techStack.entity.TechStack;
 import com.projectmatching.app.domain.user.dto.UserInfo;
 import com.projectmatching.app.domain.user.entity.User;
@@ -13,6 +14,7 @@ import org.springframework.beans.BeanUtils;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -23,7 +25,7 @@ public class TeamSimpleDto {
     private String name;
     private UserInfo userInfo;
     private String session;
-    private List<TechStack> skills;
+    private List<TechStackDto> skills;
 
     @ApiModelProperty(name = "모집 상태 여부, 모집중 / 모집완료")
     private String status;
@@ -39,6 +41,11 @@ public class TeamSimpleDto {
         teamSimpleDto.userInfo = UserInfo.of(user);
         teamSimpleDto.status = team.getStatus();
         teamSimpleDto.readCnt = Optional.ofNullable(team.getReadCnt()).orElse(0L);
+        teamSimpleDto.skills = team.getTeamTeches().stream()
+                .map(TeamTechDto::of)
+                .map(teamTechDto ->  teamTechDto.getTechStack())
+                .collect(Collectors.toList());
+
         teamSimpleDto.commentCnt = team.getTeamComments().size();
         teamSimpleDto.likeCnt = team.getTeamLikings().size();
         return teamSimpleDto;
