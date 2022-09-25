12adf6b6cff443dd7dd51b4e7363eafee2f6b714
yunho <kimyouknow@naver.com>
2022-09-25 09:13:10 +0900

    Chore: App prettier오류 수정

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -123,7 +123,6 @@ function App() {
           />
           <Route path={ROUTE.CALLBACK} element={<Callback />} />
           <Route path={ROUTE.NOTFOUND} element={<NotFound />} />
-
         </Route>
       </Routes>
     </Router>
