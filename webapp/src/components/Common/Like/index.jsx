0661ab7bf37add028ad7510ad78968c256631ba4
Wang HoEun <hoeunwang@gmail.com>
2022-09-27 23:35:06 +0900

    Fix: useAxios활용하여 코드 변경/추후 관심사의 분리 필요.

--- webapp/src/components/Common/Like/index.jsx
+++ webapp/src/components/Common/Like/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import BlueHeartIcon from 'assets/icons/blue-heart.svg';
 import FullHeartIcon from 'assets/icons/full-heart.svg';
@@ -6,6 +6,7 @@ import FullHeartIcon from 'assets/icons/full-heart.svg';
 import PropTypes from 'prop-types';
 
 import teamApi from 'api/team.api';
+import useAxios from 'hooks/useAxios';
 import * as S from './style';
 
 Like.propTypes = {
@@ -14,36 +15,23 @@ Like.propTypes = {
 
 export default function Like({ id }) {
   const [like, setLike] = useState(false);
-  const [apiError, setApiError] = useState({
-    isError: false,
-    msg: '',
+
+  const [, addteamlike] = useAxios({
+    axiosInstance: teamApi.ADD_TEAM_LIKE,
+    immediate: false,
+  });
+  const [, deleteteamlike] = useAxios({
+    axiosInstance: teamApi.DELETE_TEAM_LIKE,
+    immediate: false,
   });
 
   const toggleLike = async () => {
     // [POST] 사용자가 좋아요를 누름 -> DB 갱신
     setLike(!like);
     if (like === false) {
-      try {
-        const { data: responseData } = await teamApi.ADD_TEAM_LIKE({ id });
-        console.log('data', responseData);
-      } catch (error) {
-        console.error(error);
-        setApiError({
-          isError: true,
-          msg: error.message,
-        });
-      }
+      await addteamlike({ id });
     } else {
-      try {
-        const { data: responseData } = await teamApi.DELETE_TEAM_LIKE({ id });
-        console.log('data', responseData);
-      } catch (error) {
-        console.error(error);
-        setApiError({
-          isError: true,
-          msg: error.message,
-        });
-      }
+      await deleteteamlike({ id });
     }
   };
   return <S.Heart src={like ? FullHeartIcon : BlueHeartIcon} onClick={toggleLike} />;
