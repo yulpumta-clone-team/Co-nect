110463c959c7b00441cfdf1445ac35d94dcf0182
philos1234 <48795102+philos1234@users.noreply.github.com>
2022-10-03 23:22:30 +0900

    fixtypo

--- src/main/java/com/projectmatching/app/domain/team/repository/TeamRepository.java
+++ src/main/java/com/projectmatching/app/domain/team/repository/TeamRepository.java
@@ -2,14 +2,11 @@ package com.projectmatching.app.domain.team.repository;
 
 import com.projectmatching.app.domain.team.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-=======
 import org.springframework.stereotype.Repository;
->>>>>>> ec2cd05 (test : teamUpdateTest)
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long>, TeamRepositoryCustom {
