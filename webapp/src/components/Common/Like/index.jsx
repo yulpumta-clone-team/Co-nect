d6582a34c69c43c4480812a99c4c2b05b1206700
yunho <kimyouknow@naver.com>
2022-10-05 23:28:35 +0900

    Refactor: 좋아요 컴포넌트 수정
    네이밍 및 파일

--- webapp/src/components/Common/Like/index.jsx
+++ webapp/src/components/Common/Like/index.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
-
 import PropTypes from 'prop-types';
-
 import teamApi from 'api/team.api';
 import useAxios from 'hooks/useAxios';
-import Like from './Like';
+import HeartSvg from 'assets/icons/HeartSvg';
+import HeartFillSvg from 'assets/icons/HeartFillSvg';
+import * as S from './style';
 
-LikeApi.propTypes = {
+Like.propTypes = {
   id: PropTypes.number.isRequired,
+  initValue: PropTypes.bool.isRequired,
 };
 
-function LikeApi({ id }) {
-  const [like, setLike] = useState(false);
+export default function Like({ id, initValue }) {
+  const [isActive, setIsActive] = useState(initValue);
 
   const [, addteamlike] = useAxios({
     axiosInstance: teamApi.ADD_TEAM_LIKE,
@@ -24,15 +25,18 @@ function LikeApi({ id }) {
 
   const toggleLike = async () => {
     // [POST] 사용자가 좋아요를 누름 -> DB 갱신
-    setLike(!like);
-    if (like === false) {
-      await addteamlike({ id });
-    } else {
+    if (isActive) {
       await deleteteamlike({ id });
+      setIsActive(false);
+    } else {
+      await addteamlike({ id });
+      setIsActive(true);
     }
   };
 
-  return <Like like={like} toggleLike={toggleLike} />;
+  return (
+    <S.Heart onClick={toggleLike} isActive={isActive}>
+      {isActive ? <HeartFillSvg /> : <HeartSvg />}
+    </S.Heart>
+  );
 }
-
-export default LikeApi;
