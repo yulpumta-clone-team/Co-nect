62681b640539776a40a6f99a9e44b7050bba652f
yunho <kimyouknow@naver.com>
2022-10-12 21:52:34 +0900

    Fix: 대댓글 변수명 수정

--- webapp/src/components/ComentContainer/CommentList/Root.CommentList.jsx
+++ webapp/src/components/ComentContainer/CommentList/Root.CommentList.jsx
@@ -11,8 +11,7 @@ RootCommentList.propTypes = {
 export default function RootCommentList({ postWriter, comments }) {
   return (
     <S.RootListContainer>
-      {/* FIXME: api명세가 replies에서 comments로 바뀌어서 임시로 수정해 놓음 */}
-      {comments.map(({ id, teamId, userId, comments: replies, ...commentInfo }) => (
+      {comments.map(({ id, teamId, userId, replies, ...commentInfo }) => (
         <RootCommentElement
           key={id}
           commentInfo={{ ...commentInfo, id }}
