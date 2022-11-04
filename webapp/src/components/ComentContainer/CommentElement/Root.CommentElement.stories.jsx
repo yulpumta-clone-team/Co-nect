7fdd44c732b688ea66a651f0485948c7d44a20a9
yunho <kimyouknow@naver.com>
2022-11-04 23:12:39 +0900

    Story: 댓글 스토리 mock데이터 수정

--- webapp/src/components/ComentContainer/CommentElement/Root.CommentElement.stories.jsx
+++ webapp/src/components/ComentContainer/CommentElement/Root.CommentElement.stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CommentProvider from 'contexts/Comment/Comment.Provider';
+import { userComment } from 'mocks/commentHandler.mock/userComments';
 import RootCommentElement from './Root.CommentElement';
 
 export default {
@@ -19,76 +20,18 @@ function Template(args) {
 
 export const Default = Template.bind({});
 Default.args = {
-  commentInfo: {
-    id: 999,
-    img: '',
-    writer: '수정된',
-    parentId: null,
-    secret: false,
-    content: '수정된 연습용 댓긇',
-    feeling: [901092146501, 92719182999999, 927191821029312],
-  },
-  postWriter: 'front_temp',
-  replies: [],
+  postWriterId: 9021279491,
+  commentInfo: { ...userComment, secret: false, replies: [] },
 };
 
 export const Secret = Template.bind({});
 Secret.args = {
-  commentInfo: {
-    id: 999,
-    img: '',
-    writer: '수정된',
-    parentId: null,
-    secret: true,
-    content: '수정된 연습용 댓긇',
-    feeling: [901092146501, 92719182999999, 927191821029312],
-  },
-  postWriter: 'front_temp',
-  replies: [],
+  postWriterId: 9021279491,
+  commentInfo: { ...userComment, secret: true, replies: [] },
 };
 
 export const WithReplies = Template.bind({});
 WithReplies.args = {
-  commentInfo: {
-    id: 999,
-    img: '',
-    writer: '수정된',
-    parentId: null,
-    secret: false,
-    content: '수정된 연습용 댓긇',
-    feeling: [901092146501, 92719182999999, 927191821029312],
-  },
-  postWriter: 'front_temp',
-  replies: [
-    {
-      id: 1012,
-      img: '',
-      userId: 123137777,
-      writer: 'front_temp',
-      parentId: 999,
-      secret: true,
-      content: '연습용',
-      feeling: [901092146501],
-    },
-    {
-      id: 12,
-      img: '',
-      userId: 12512512,
-      writer: 'string',
-      parentId: 999,
-      secret: false,
-      content: '수정된',
-      feeling: [784833333333, 19148710],
-    },
-    {
-      id: 10,
-      img: '',
-      userId: 12512512,
-      writer: 'front_temp',
-      parentId: 999,
-      secret: false,
-      content: '연습용',
-      feeling: [901092146501],
-    },
-  ],
+  postWriterId: 9021279491,
+  commentInfo: { ...userComment, secret: false },
 };
