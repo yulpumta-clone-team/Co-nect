887b193737d0585d5c8e371827f2debffdd4d732
yunho <kimyouknow@naver.com>
2022-11-06 23:04:49 +0900

    Fix: ProfileImage컴포넌트 alt, src 기본값 추가

--- webapp/src/components/ProfileImg/style.js
+++ webapp/src/components/ProfileImg/style.js
@@ -1,14 +1,9 @@
 import styled from 'styled-components';
 
 export const ProfileImg = styled.img`
-  position: absolute; // * : 각 div 상자에 걸쳐있는 프로필 이미지 때문에 absolute로 위치를 설정해 줌.
-  display: flex;
-
-  width: 30%;
-  height: 25%;
-
-  left: 62%;
-  top: 18%;
-
+  width: 100%;
+  height: 100%;
   border-radius: 50%;
+
+  ${({ customStyle }) => customStyle}
 `;
