1e8090b1e7174ca43d798cd271f96aa43981b0b6
yunho <kimyouknow@naver.com>
2022-11-10 23:04:49 +0900

    Feat: OAuthCallback

--- webapp/src/pages/OAuthCallback/index.jsx
+++ webapp/src/pages/OAuthCallback/index.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
+import queryString from 'query-string';
+import { useLocation } from 'react-router-dom';
+import useHandleLogin from 'hooks/useHandleLogin';
 
-OAuthCallback.propTypes = {};
+export default function OAuthCallback({}) {
+  const { search } = useLocation();
+  const { accessToken, refreshToken, isFirst: isFirstLogin } = queryString.parse(search);
+  const { handleLogin } = useHandleLogin();
 
-export default function OAuthCallback(props) {
-  return <div>OAuthCallback</div>;
+  useEffect(() => {
+    handleLogin({ accessToken, refreshToken, isFirstLogin });
+  }, []);
+
+  return <div>임시</div>;
 }
