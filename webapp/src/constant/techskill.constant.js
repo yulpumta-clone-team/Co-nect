70b8a63acc00157f401704b90d07a17f43ed78de
yunho <kimyouknow@naver.com>
2022-12-23 15:14:42 +0900

    Refactor: 기술스택 배열 constant파일 별도로 분리

---
+++ webapp/src/constant/techskill.constant.js
@@ -0,0 +1,234 @@
+import js_100 from 'assets/images/techskillls/100-js.png';
+import react_101 from 'assets/images/techskillls/101-react.png';
+import vue_102 from 'assets/images/techskillls/102-vue.png';
+import angular_103 from 'assets/images/techskillls/103-angular.png';
+import svelte_104 from 'assets/images/techskillls/104-svelte.png';
+import ts_105 from 'assets/images/techskillls/105-ts.png';
+import nextjs_106 from 'assets/images/techskillls/106-nextjs.png';
+import nodejs_200 from 'assets/images/techskillls/200-nodejs.png';
+import nestjs_201 from 'assets/images/techskillls/201-nestjs.png';
+import express_202 from 'assets/images/techskillls/202-express.png';
+import java_203 from 'assets/images/techskillls/203-java.png';
+import spring_204 from 'assets/images/techskillls/204-spring.png';
+import c_205 from 'assets/images/techskillls/205-c.png';
+import cpp_206 from 'assets/images/techskillls/206-cpp.png';
+import cs_207 from 'assets/images/techskillls/207-cs.png';
+import python_208 from 'assets/images/techskillls/208-python.png';
+import django_209 from 'assets/images/techskillls/209-django.png';
+import go_210 from 'assets/images/techskillls/210-go.png';
+import php_211 from 'assets/images/techskillls/211-php.png';
+import flutter_300 from 'assets/images/techskillls/300-flutter.png';
+import reactnative_301 from 'assets/images/techskillls/301-reactnative.png';
+import kotlin_302 from 'assets/images/techskillls/302-kotlin.png';
+import swift_303 from 'assets/images/techskillls/303-swift.png';
+import mysql_500 from 'assets/images/techskillls/500-mysql.png';
+import h2_501 from 'assets/images/techskillls/501-h2.png';
+import mongodb_502 from 'assets/images/techskillls/502-mongodb.png';
+import oracle_503 from 'assets/images/techskillls/503-oracle.png';
+import git_600 from 'assets/images/techskillls/600-git.png';
+import docker_601 from 'assets/images/techskillls/601-docker.png';
+import kubernetes_602 from 'assets/images/techskillls/602-kubernetes.png';
+import aws_603 from 'assets/images/techskillls/603-aws.png';
+import jest_604 from 'assets/images/techskillls/604-jest.png';
+import cypress_605 from 'assets/images/techskillls/605-cypress.png';
+
+export const TECH_SKILLS = [
+  {
+    key: 100,
+    category: 'front',
+    techName: 'javascript',
+    image: js_100,
+  },
+  {
+    key: 101,
+    category: 'front',
+    techName: 'react',
+    image: react_101,
+  },
+  {
+    key: 102,
+    category: 'front',
+    techName: 'vue',
+    image: vue_102,
+  },
+  {
+    key: 103,
+    category: 'front',
+    techName: 'angular',
+    image: angular_103,
+  },
+  {
+    key: 104,
+    category: 'front',
+    techName: 'svelte',
+    image: svelte_104,
+  },
+  {
+    key: 105,
+    category: 'front',
+    techName: 'typescript',
+    image: ts_105,
+  },
+  {
+    key: 106,
+    category: 'front',
+    techName: 'nextjs',
+    image: nextjs_106,
+  },
+  {
+    key: 200,
+    category: 'back',
+    techName: 'nodeJs',
+    image: nodejs_200,
+  },
+  {
+    key: 201,
+    category: 'back',
+    techName: 'nestJs',
+    image: nestjs_201,
+  },
+  {
+    key: 202,
+    category: 'back',
+    techName: 'express',
+    image: express_202,
+  },
+  {
+    key: 203,
+    category: 'back',
+    techName: 'java',
+    image: java_203,
+  },
+  {
+    key: 204,
+    category: 'back',
+    techName: 'spring',
+    image: spring_204,
+  },
+  {
+    key: 205,
+    category: 'back',
+    techName: 'C',
+    image: c_205,
+  },
+  {
+    key: 206,
+    category: 'back',
+    techName: 'C++',
+    image: cpp_206,
+  },
+  {
+    key: 207,
+    category: 'back',
+    techName: 'C#',
+    image: cs_207,
+  },
+  {
+    key: 208,
+    category: 'back',
+    techName: 'python',
+    image: python_208,
+  },
+  {
+    key: 209,
+    category: 'back',
+    techName: 'django',
+    image: django_209,
+  },
+  {
+    key: 210,
+    category: 'back',
+    techName: 'go',
+    image: go_210,
+  },
+  {
+    key: 211,
+    category: 'back',
+    techName: 'php',
+    image: php_211,
+  },
+  {
+    key: 300,
+    category: 'mobile',
+    techName: 'flutter',
+    image: flutter_300,
+  },
+  {
+    key: 301,
+    category: 'mobile',
+    techName: 'reactNative',
+    image: reactnative_301,
+  },
+  {
+    key: 302,
+    category: 'mobile',
+    techName: 'kotlin',
+    image: kotlin_302,
+  },
+  {
+    key: 303,
+    category: 'mobile',
+    techName: 'swift',
+    image: swift_303,
+  },
+  {
+    key: 400,
+    category: 'DB',
+    techName: 'mysql',
+    image: mysql_500,
+  },
+  {
+    key: 401,
+    category: 'DB',
+    techName: 'h2',
+    image: h2_501,
+  },
+  {
+    key: 402,
+    category: 'DB',
+    techName: 'mongodb',
+    image: mongodb_502,
+  },
+  {
+    key: 403,
+    category: 'DB',
+    techName: 'oracle',
+    image: oracle_503,
+  },
+  {
+    key: 500,
+    category: 'arichitecture',
+    techName: 'git',
+    image: git_600,
+  },
+  {
+    key: 501,
+    category: 'arichitecture',
+    techName: 'docker',
+    image: docker_601,
+  },
+  {
+    key: 502,
+    category: 'arichitecture',
+    techName: 'kubernetes',
+    image: kubernetes_602,
+  },
+  {
+    key: 503,
+    category: 'arichitecture',
+    techName: 'aws',
+    image: aws_603,
+  },
+  {
+    key: 504,
+    category: 'arichitecture',
+    techName: 'jest',
+    image: jest_604,
+  },
+  {
+    key: 505,
+    category: 'arichitecture',
+    techName: 'cypress',
+    image: cypress_605,
+  },
+];
