0c5f4b675a09c4d06bf48d87247baefd7f150723
yunho <kimyouknow@naver.com>
2022-12-26 18:11:36 +0900

    Story: 유저.팀 빈 페이지 추가

--- webapp/src/pages/TeamBoard/TeamBoard.stories.jsx
+++ webapp/src/pages/TeamBoard/TeamBoard.stories.jsx
@@ -1,3 +1,7 @@
+import CardsGrid from 'components/CardsGrid';
+import TeamCard from 'components/TeamCard';
+import { ROUTE } from 'constant/route.constant';
+import { emptyTrigger } from 'constant/service.constant';
 import handlers from 'mocks/handlers';
 import React from 'react';
 import TeamBoard from './index';
@@ -15,5 +19,19 @@ function Template(args) {
   return <TeamBoard {...args} />;
 }
 
+function EmptyTemplate() {
+  return (
+    <CardsGrid
+      CardComponent={TeamCard}
+      cards={[]}
+      clickLink={`${ROUTE.TEAM}/`}
+      emptyTrigger={emptyTrigger.team}
+    />
+  );
+}
+
 export const Default = Template.bind({});
 Default.args = {};
+
+export const Empty = EmptyTemplate.bind({});
+Empty.args = {};
