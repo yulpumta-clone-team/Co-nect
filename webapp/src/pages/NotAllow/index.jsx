02e43efc8a97af8d66381f9d4e68292f32eadc26
yunho <kimyouknow@naver.com>
2022-12-28 15:22:24 +0900

    Style: NotAllow페이지

--- webapp/src/pages/NotAllow/index.jsx
+++ webapp/src/pages/NotAllow/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import styled, { css } from 'styled-components/macro';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
+import Button from 'components/Common/Button';
 
 NotAllow.propTypes = {
   warnMessage: PropTypes.string.isRequired,
@@ -12,12 +14,29 @@ export default function NotAllow({ warnMessage }) {
     navigate('/');
   };
   return (
-    <div>
-      NotAllow
-      <h1>{warnMessage}</h1>
-      <button variant="contained" onClick={backToMain}>
+    <S.Container>
+      <h1>NotAllow</h1>
+      <h3>{warnMessage}</h3>
+      <Button theme="primary" onClick={backToMain} customStyle={S.SButton}>
         홈으로 돌아가기
-      </button>
-    </div>
+      </Button>
+    </S.Container>
   );
 }
+
+const Container = styled.div`
+  max-width: 1180px;
+  height: 100vh;
+  ${({ theme: { mixin } }) => mixin.flexCenter({})}
+  gap: 24px;
+`;
+
+const SButton = css`
+  width: 200px;
+  height: 35px;
+`;
+
+const S = {
+  Container,
+  SButton,
+};
