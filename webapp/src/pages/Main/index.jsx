e066ea5509bdf69d18c07505a480d9c7b663a190
yunho <kimyouknow@naver.com>
2022-12-28 15:43:13 +0900

    Refactor: 메인페이지에서 cardText, links를 constant로 분리

--- webapp/src/pages/Main/index.jsx
+++ webapp/src/pages/Main/index.jsx
@@ -1,43 +1,22 @@
-import { benefits, developers } from 'constant/main.constant';
-import { ROUTE } from 'constant/route.constant';
 import React from 'react';
+import { benefits, cardText, developers, links } from 'constant/main.constant';
 import { useNavigate } from 'react-router-dom';
 import * as S from './style';
 
+const IconMap = {
+  signup: <S.SignUp />,
+  board: <S.Board />,
+  post: <S.Post />,
+  cloud: <S.Cloud />,
+  ghost: <S.Ghost />,
+  earth: <S.Earth />,
+  heartPoint: <S.HeartPoint />,
+  cellPhone: <S.CellPhone />,
+  message: <S.Message />,
+};
+
 export default function Main() {
   const navigate = useNavigate();
-  const links = [
-    { icon: <S.SignUp />, route: ROUTE.SIGN_UP },
-    { icon: <S.Board />, route: ROUTE.TEAM },
-    { icon: <S.Post />, route: ROUTE.NEW_POST },
-  ];
-  const cardText = [
-    {
-      spanFirst: '코넥트는',
-      spanSecond: '이런 어플입니다.',
-      default:
-        '한 페이지에서 나와 같은 목표를 가진 팀원을 만나보세요! 다양한 사람들과 함께 팀을 구성해보세요.목표가 같은 코넥티를 찾아 프로젝트를 진행해봐요!',
-      iconFront: <S.Cloud />,
-      iconBehind: <S.Ghost />,
-    },
-    {
-      spanFirst: ' 내가 원하는',
-      spanSecond: '코넥티만을 골라서',
-      default:
-        ' 수많은 코넥티 중 기술, 직업, 희망 작업 기간 등을 필터링해 내 프로젝트에 딱 맞는 코넥티를 빠르고 쉽게 서치할 수 있습니다.',
-      iconFront: <S.Earth />,
-      iconBehind: <S.HeartPoint />,
-    },
-    {
-      spanFirst: '한 페이지에서',
-      spanSecond: '카드를 통해 빠르게',
-      default:
-        '프로젝트에 필수적인 정보만 보고 싶다! 모집분야, 기술스택 진행기간 등의 프로젝트 정보를 심플한 카드 형식에 담아 한눈에 볼 수 있습니다.',
-      iconFront: <S.CellPhone />,
-      iconBehind: <S.Message />,
-    },
-  ];
-
   return (
     <S.MainContainer>
       <S.MainSection>
@@ -64,10 +43,10 @@ export default function Main() {
         </S.StartQuestion>
         <S.Benefit>
           <S.CircleGroup>
-            {benefits.map((benefit, index) => (
-              <S.Circle>
-                <span>{benefit.first}</span>
-                {benefit.second}
+            {benefits.map(({ first, second }) => (
+              <S.Circle key={first}>
+                <span>{first}</span>
+                {second}
               </S.Circle>
             ))}
           </S.CircleGroup>
@@ -86,19 +65,19 @@ export default function Main() {
         </S.Subject>
       </S.PurposeSection>
       <S.FunctionSection>
-        {cardText.map((text, index) => (
-          <S.Card>
-            {text.iconFront}
-            {text.iconBehind}
+        {cardText.map(({ spanFirst, spanSecond, iconFront, iconBehind, defaults }) => (
+          <S.Card key={spanFirst}>
+            {IconMap[iconFront]}
+            {IconMap[iconBehind]}
             <S.CardText>
               <p>
                 <span>
-                  {text.spanFirst}
+                  {spanFirst}
                   <br />
-                  {text.spanSecond}
+                  {spanSecond}
                 </span>
                 <br />
-                {text.default}
+                {defaults}
               </p>
             </S.CardText>
           </S.Card>
@@ -107,16 +86,18 @@ export default function Main() {
       <S.FinishSection>
         <span>코넥티들을 위한 도전은 계속됩니다.</span>
         <S.LinkGroup>
-          {links.map((link, index) => (
-            <S.GotoLink onClick={() => navigate(link.route)}>{link.icon}</S.GotoLink>
+          {links.map(({ icon, route }) => (
+            <S.GotoLink key={icon} onClick={() => navigate(route)}>
+              {IconMap[icon]}
+            </S.GotoLink>
           ))}
         </S.LinkGroup>
       </S.FinishSection>
       <S.BottomBox>
-        {developers.map((developer, index) => (
-          <S.InformationBox>
+        {developers.map(({ name, field, email }) => (
+          <S.InformationBox key={name}>
             <p>
-              <span>{developer.name}</span> {developer.field} {developer.email}
+              <span>{name}</span> {field} {email}
             </p>
           </S.InformationBox>
         ))}
