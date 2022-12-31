dfb7271c549f7a1e61743d2cad9820ff7e413a79
yunho <kimyouknow@naver.com>
2022-12-31 15:46:25 +0900

    Feat: 댓글 컴포넌트 에러 UI

--- webapp/src/components/ComentContainer/LoadingCommentList.jsx
+++ webapp/src/components/ComentContainer/LoadingCommentList.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useCommentsAction, useCommentsState } from 'contexts/Comment/Comment.Provider';
+import Button from 'components/Common/Button';
 import RootCommentList from './CommentList/Root.CommentList';
+import * as S from './style';
 
 HocLoadingCommentList.propTypes = {
   postWriterId: PropTypes.number.isRequired,
@@ -15,10 +17,12 @@ export default function HocLoadingCommentList({ postWriterId }) {
 
   if (apiError.isError)
     return (
-      <div>
-        <h1>{apiError.msg}</h1>
-        <button onClick={forceRefetch}>refetch</button>
-      </div>
+      <S.ErrorContainer>
+        <h4>{apiError.msg}</h4>
+        <Button theme="primary" onClick={forceRefetch}>
+          다시 요청해주세요.
+        </Button>
+      </S.ErrorContainer>
     );
   if (!comments && comments.length !== 0) return <div>댓글이 없어요.</div>;
   return <RootCommentList postWriterId={postWriterId} comments={comments} />;
