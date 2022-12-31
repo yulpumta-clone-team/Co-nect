a48d6ae092e9dd6c568425a7a4335f3eb6b00ca5
yunho <kimyouknow@naver.com>
2022-12-31 20:47:25 +0900

    Refactor: 이미지 공통 컴포넌트 s3 적용

--- webapp/src/components/ProfileImg/index.jsx
+++ webapp/src/components/ProfileImg/index.jsx
@@ -1,19 +1,19 @@
 import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import DefaultProfileImage from 'assets/images/default-profile.png';
-
+import { S3IMAGE_URL } from 'utils';
 import * as S from './style';
 
-const ProfileImg = forwardRef(({ src, alt, customStyle }, ref) => {
+const ProfileImg = forwardRef(({ src, alt, customStyle, isAssets = false }, ref) => {
   const handleError = (event) => {
     event.target.onerror = null;
     event.target.src = DefaultProfileImage;
   };
-  const defaultSrc = src || DefaultProfileImage;
+  const imageSrc = isAssets ? src : S3IMAGE_URL(src);
   return (
     <S.ProfileImg
       ref={ref}
-      src={defaultSrc}
+      src={imageSrc}
       alt={alt}
       customStyle={customStyle}
       onError={handleError}
@@ -25,6 +25,7 @@ ProfileImg.propTypes = {
   src: PropTypes.string,
   alt: PropTypes.string.isRequired,
   customStyle: PropTypes.array,
+  isAssets: PropTypes.bool,
 };
 
 export default ProfileImg;
