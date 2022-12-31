3b6fc62bb8e87d77a3cf9dc839c2ecf020472ffa
yunho <kimyouknow@naver.com>
2022-12-31 20:47:57 +0900

    Refactor: SimpleListComponent proptypes 추가

--- webapp/src/hoc/SimpleListComponent.jsx
+++ webapp/src/hoc/SimpleListComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 SimpleListComponent.propTypes = {
-  Component: PropTypes.element.isRequired,
+  Component: PropTypes.oneOfType([PropTypes.element, PropTypes.func, PropTypes.object]).isRequired,
   idx: PropTypes.number.isRequired,
 };
 /**
@@ -11,7 +11,10 @@ SimpleListComponent.propTypes = {
  * @param {number} idx 반복횟수
  */
 export default function SimpleListComponent({ Component, idx }) {
-  return Array(idx)
-    .fill(0)
-    .map((_, i) => <Component idx={i} />);
+  return (
+    Array(idx)
+      .fill(0)
+      // eslint-disable-next-line react/no-array-index-key
+      .map((_, i) => <Component key={i} />)
+  );
 }
