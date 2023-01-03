4272c2d713d6749845ba89cc56b9036f1f1cb4c8
yunho <kimyouknow@naver.com>
2023-01-03 17:23:53 +0900

    Refactor : MYList 리팩토링

---
+++ webapp/src/pages/MyList/MyList.style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+  width: 100%;
+  margin: 0 auto;
+`;
+
+export const SessionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  width: 100%;
+  padding: 0 16px 16px 16px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+  margin-bottom: 24px;
+`;
--- webapp/src/pages/MyList/index.jsx
+++ webapp/src/pages/MyList/index.jsx
@@ -1,58 +1,79 @@
-import React, { useState } from 'react';
-import Cards from 'components/CardsGrid';
+import React, { useEffect, useState } from 'react';
 import teamApi from 'api/team.api';
 import userApi from 'api/user.api';
 import Tabs from 'components/Common/Tabs';
 import UserCard from 'components/UserCard';
 import TeamCard from 'components/TeamCard';
 import { ROUTE } from 'constant/route.constant';
-import WithLoading from 'hoc/WithLoading';
-import * as S from './style';
+import WithInfiniteScroll from 'hoc/WithInfiniteScroll';
+import { emptyTrigger } from 'constant/service.constant';
+import Divider from 'components/Common/Divider';
+import * as S from './MyList.style';
 
 export default function MyList() {
-  const [listTabId, setListTabId] = useState(LIKES_ID);
-  const [postTabId, setPostTabId] = useState(USER_ID);
+  const [postType, setPostType] = useState(LIKES_ID); // 좋아요 or 읽은 목록
+  const [domainType, setDomainType] = useState(USER_ID); // 유저 or 팀
 
-  const isUserList = postTabId === USER_ID;
+  const isUserList = domainType === USER_ID;
   const CardComponent = isUserList ? UserCard : TeamCard;
   const clickLink = isUserList ? ROUTE.USER : ROUTE.TEAM;
+  const emtpyTriggerType = isUserList ? emptyTrigger.user : emptyTrigger.team;
 
-  const activedFetcher = fetcherObj[postTabId][listTabId];
+  const onClickTab = (type, id) => {
+    if (type === TAB_TYPE.domain) {
+      setDomainType(id);
+    } else {
+      setPostType(id);
+    }
+  };
 
-  const CardsView = WithLoading({
-    Component: Cards,
-    responseDataKey: 'cards',
-    axiosInstance: activedFetcher,
-    axiosConfig: {},
-  });
-  // * <Tabs tabs={POST_TYPE_TABS} activeTabId={postTabId} setActiveTab={setPostTabId} /> 이거는 사이드에 넣어주기!
   return (
     <S.Container>
       <S.SessionContainer>
-        <Tabs tabs={LIST_TYPE_TABS} activeTabId={listTabId} setActiveTab={setListTabId} />
+        <Tabs
+          type={TAB_TYPE.domain}
+          tabs={DOMAIN_TYPE_TABS}
+          activeId={domainType}
+          onClickTab={onClickTab}
+        />
+        <Divider />
+        <Tabs
+          type={TAB_TYPE.post}
+          tabs={POST_TYPE_TABS}
+          activeId={postType}
+          onClickTab={onClickTab}
+        />
       </S.SessionContainer>
-      <S.BoardContainer>
-        <CardsView CardComponent={CardComponent} clickLink={`${clickLink}/`} />
-      </S.BoardContainer>
+      <WithInfiniteScroll
+        key={`${domainType}-${postType}`} // 자식컴포넌트의 상태를 강제로 reset하기 위한 조치
+        CardComponent={CardComponent}
+        clickLink={clickLink}
+        axiosInstance={fetcherObj[domainType][postType]}
+        emptyTrigger={emtpyTriggerType}
+      />
     </S.Container>
   );
 }
 
-const LIKES_ID = 'like';
-const READS_ID = 'read';
-const USER_ID = 'user';
-const TEAM_ID = 'team';
+const TAB_TYPE = {
+  domain: 'domain',
+  post: 'post',
+};
 
-const LIST_TYPE_TABS = [
+const LIKES_ID = 'LIKE';
+const READS_ID = 'READ';
+const USER_ID = 'USER';
+const TEAM_ID = 'TEAM';
+
+const POST_TYPE_TABS = [
   { id: LIKES_ID, title: '내가 좋아요 한 글' },
   { id: READS_ID, title: '내가 읽은 글' },
 ];
 
-// const POST_TYPE_TABS = [
-//   { id: USER_ID, title: '유저' },
-//   { id: TEAM_ID, title: '팀' },
-// ];
-
+const DOMAIN_TYPE_TABS = [
+  { id: USER_ID, title: '유저' },
+  { id: TEAM_ID, title: '팀' },
+];
 const fetcherObj = {
   [USER_ID]: {
     [LIKES_ID]: userApi.GET_USER_LIKES,
@@ -63,3 +84,5 @@ const fetcherObj = {
     [READS_ID]: teamApi.GET_TEAM_READS,
   },
 };
+
+const INIT_FETCHER = fetcherObj[USER_ID][LIKES_ID];
