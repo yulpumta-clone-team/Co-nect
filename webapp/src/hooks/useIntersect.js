cd729a5e484bc19137fff3f9af640153f31bc346
yunho <kimyouknow@naver.com>
2023-01-06 14:44:01 +0900

    Fix: intersection api 교차상태 0.5 -> 0.1로 수정

--- webapp/src/hooks/useIntersect.js
+++ webapp/src/hooks/useIntersect.js
@@ -1,9 +1,9 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 const defaultOption = {
   root: null,
   rootMargin: '0px',
-  threshold: 0.5,
+  threshold: 0.1,
 };
 
 // FIXME: react hooks return 타입을 jsdoc로 만들 수 없다.. 추론이 안되네요. object로 변경 필요
@@ -18,7 +18,7 @@ const defaultOption = {
  */
 
 /**
- * IntersectionObserver를 사용하기 위한 custom hoooks
+ * IntersectionObserver를 사용하기 위한 custom hooks
  * @param {callback} callback 바라보고 있는 요소가 교차할 때(isIntersecting) 실행할 callback함수
  * @param {Object} customOption  IntersectionObserver인스턴스를 생성하기 위한 option (root, rootMargin,threshold)  https://developer.mozilla.org/ko/docs/Web/API/IntersectionObserver/IntersectionObserver
  * @returns {useIntersectReturns}  useIntersect를 사용하는 곳에서 사용할 method 및 state
@@ -26,7 +26,7 @@ const defaultOption = {
 export default function useIntersect(callback, customOption) {
   const loadMoreRef = useRef(null);
 
-  const handleObsever = async ([entry], observer) => {
+  const handleObserver = async ([entry], observer) => {
     if (entry.isIntersecting) {
       observer.unobserve(entry.target);
       await callback();
@@ -37,7 +37,7 @@ export default function useIntersect(callback, customOption) {
   useEffect(() => {
     let observer;
     if (loadMoreRef.current) {
-      observer = new IntersectionObserver(handleObsever, { ...defaultOption, ...customOption });
+      observer = new IntersectionObserver(handleObserver, { ...defaultOption, ...customOption });
       observer.observe(loadMoreRef.current);
     }
     return () => observer && observer.current && observer.disconnect();
