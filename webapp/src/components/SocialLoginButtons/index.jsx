f07aba069f2844437098e8a2ba2888653a9d2739
yunho <kimyouknow@naver.com>
2023-01-06 21:56:51 +0900

    Feat: 소셜로그인 타입 CONSTANT 변수 생성
    - 구글
    - 깃허브

--- webapp/src/components/SocialLoginButtons/index.jsx
+++ webapp/src/components/SocialLoginButtons/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import googleLogo from 'assets/images/google-logo.png';
 import githubLogo from 'assets/images/github-logo.png';
-import { OAUTH_URL } from 'constant/route.constant';
+import { OAUTH_TYPE, OAUTH_URL } from 'constant/route.constant';
 import Image from 'components/Common/Image';
 import * as S from './SocialLoginButtons.style';
 
@@ -13,10 +13,10 @@ export default function SocialLoginButtons() {
     <S.Container>
       <span>소셜계정으로 로그인</span>
       <S.Buttons>
-        <S.Button onClick={() => handleClickSocialLogin('GOOGLE')}>
-          <Image src={googleLogo} alt="googleLogo" isAssets />
+        <S.Button onClick={() => handleClickSocialLogin(OAUTH_TYPE.GOOGLE)}>
+          <Image src={googleLogo} alt="google logo" isAssets />
         </S.Button>
-        <S.Button onClick={() => handleClickSocialLogin('GITHUB')}>
+        <S.Button onClick={() => handleClickSocialLogin(OAUTH_TYPE.GITHUB)}>
           <Image src={githubLogo} alt="github logo" isAssets />
         </S.Button>
       </S.Buttons>
